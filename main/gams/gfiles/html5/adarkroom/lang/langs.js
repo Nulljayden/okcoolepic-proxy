21a02ab96e15bdef4897d5d359372c00fd6189ad
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:16 -0500

    comment

--- main/gams/gfiles/html5/adarkroom/lang/langs.js
+++ main/gams/gfiles/html5/adarkroom/lang/langs.js
@@ -1,28 +1,39 @@
-var langs = {
-	'cs':'czech',
-	'de':'deutsch',
-	'el':'ελληνικά',
-	'en':'english',
-	'eo':'esperanto',
-	'es':'español',
-	'fr':'français',
-	'gl':'galego',
-	'id':'bahasa indonesia',
-	'it':'italiano',
-	'lv':'latviešu valoda',
-	'ja':'日本語',
-	'ko':'한국어',
-	'nb':'norsk',
-	'pl':'polski',
-	'lt_LT':'lietuvių',
-	'pt':'português',
-	'pt_br':'português (brasil)',
-	'ru':'русский',
-	'sv':'svenska',
-	'th':'ไทย',
-	'tr':'türkçe',
-	'uk':'українська',
-	'vi':'tiếng việt',
-	'zh_cn':'简体中文',
-	'zh_tw':'繁體中文'
+// This object, `langs`, is a mapping of language codes to their corresponding
+// language names. The language codes are abbreviated strings that represent
+// different languages, while the language names are the full names of those
+// languages in their respective languages.
+//
+// For example, the key 'cs' maps to the value 'czech', which means that the
+// language code 'cs' corresponds to the Czech language.
+//
+// This object can be used to look up the full name of a language given its
+// abbreviated code, or to get the abbreviated code for a language given its
+// full name.
+const langs = {
+  'cs':'czech',
+  'de':'deutsch',
+  'el':'ελληνικά',
+  'en':'english',
+  'eo':'esperanto',
+  'es':'español',
+  'fr':'français',
+  'gl':'galego',
+  'id':'bahasa indonesia',
+  'it':'italiano',
+  'lv':'latviešu valoda',
+  'ja':'日本語',
+  'ko':'한국어',
+  'nb':'norsk',
+  'pl':'polski',
+  'lt_LT':'lietuvių',
+  'pt':'português',
+  'pt_br':'português (brasil)',
+  'ru':'русский',
+  'sv':'svenska',
+  'th':'ไทย',
+  'tr':'türkçe',
+  'uk':'українська',
+  'vi':'tiếng việt',
+  'zh_cn':'简体中文',
+  'zh_tw':'繁體中文'
 };
