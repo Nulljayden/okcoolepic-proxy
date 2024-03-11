3218c450b5ab50bd757ae3b08f2d3ee25b02e50a
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:47 -0500

    improve

--- main/gams/gfiles/html5/sleepingbeauty/rollup.config.js
+++ main/gams/gfiles/html5/sleepingbeauty/rollup.config.js
@@ -1,14 +1,16 @@
 "use strict";
 
-var includePaths = require("rollup-plugin-includepaths");
+const includePaths = require("rollup-plugin-includepaths");
 
-var includePathsOptions = {
-	paths: ["src/js"]
+const includePathsOptions = {
+  paths: ["src/js"]
 };
 
 module.exports = {
-    entry: "src/js/app.js",
-    dest: "src/app.rollup.js",
-    format: "iife",
-    plugins: [ includePaths(includePathsOptions) ]
+  input: "src/js/app.js",
+  output: {
+    file: "src/app.rollup.js",
+    format: "iife"
+  },
+  plugins: [includePaths(includePathsOptions)]
 };
