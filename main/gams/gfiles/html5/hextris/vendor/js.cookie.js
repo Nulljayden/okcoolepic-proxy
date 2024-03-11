96cd4a2aa0185549fa2623e74c37e903d6398cab
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:37 -0500

    comment

--- main/gams/gfiles/html5/hextris/vendor/js.cookie.js
+++ main/gams/gfiles/html5/hextris/vendor/js.cookie.js
@@ -1,5 +1,23 @@
 /*!
  * JavaScript Cookie v2.1.0
+ * A simple, lightweight JavaScript library for working with cookies.
  * https://github.com/js-cookie/js-cookie
  *
- * Copyright 2
+ * Copyright 2015, Klaus Hartl & Fagner Brack
+ * Released under the MIT license.
+ *
+ * Usage:
+ *  - $.cookie(name, value, options)
+ *  - $.cookie(name)
+ *  - $.removeCookie(name, options)
+ *
+ * Example:
+ *  // Set a cookie
+ *  $.cookie('name', 'value', { expires: 7, path: '/' });
+ *
+ *  // Get a cookie
+ *  $.cookie('name'); // => 'value'
+ *
+ *  // Delete a cookie
+ *  $.removeCookie('name');
+ */
