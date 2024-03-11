b159c97fc01cc86ab6d4c94507092e7cf3f0f66d
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:02 -0500

    comment

--- main/gams/gfiles/html5/tetris/PreviewGroup.js
+++ main/gams/gfiles/html5/tetris/PreviewGroup.js
@@ -1,47 +1,47 @@
-
+// PreviewGroup function creates a new group of 4 blocks with a specific shape and color
 function PreviewGroup(baseX, baseY) {
-    var i;
-    
+    // Initialize the blocks array and shape variable
     this.blocks = [];
     this.shape = null;
 
-    // create the blocks
-    for (i = 0; i < 4; i += 1) {
-	this.blocks.push(new Block({
-	    boardOriginX: baseX,
-	    boardOriginY: baseY,
-	    blockX: 0,
-	    blockY: 0,
-	    shape: 'i'
-	}));
+    // Create the blocks using a for loop
+    for (let i = 0; i < 4; i += 1) {
+        this.blocks.push(new Block({
+            boardOriginX: baseX,
+            boardOriginY: baseY,
+            blockX: 0,
+            blockY: 0,
+            shape: 'i'
+        }));
     }
 }
 
-/**
-* Sets the shape and color of the blocks
-* @param {Char} shape - the letter of the new shape
-* @param {Boolean} preview - true if it should have preview colors
-*/
-PreviewGroup.prototype.setShape = function(shape) {
-    var shapeConfig = SHAPES[shape],
-    i;
+// The setShape method sets the shape and color of the blocks in the group
+PreviewGroup.prototype.setShape = function(shape, preview) {
+    // Get the shape configuration from the SHAPES object
+    const shapeConfig = SHAPES[shape];
+    let i;
 
+    // Set the shape variable to the new shape
     this.shape = shape;
-    
+
+    // Set the position and color of each block in the group using a for loop
     for (i = 0; i < 4; i += 1) {
-	this.blocks[i].setPosition(shapeConfig.pos[i].x, shapeConfig.pos[i].y);
-	this.blocks[i].setColor(shape, false);
+        this.blocks[i].setPosition(shapeConfig.pos[i].x, shapeConfig.pos[i].y);
+        this.blocks[i].setColor(shape, preview);
     }
 };
 
+// The getShape method returns the current shape of the group
 PreviewGroup.prototype.getShape = function () {
     return this.shape;
 };
 
+// The draw method draws each block in the group on the canvas
 PreviewGroup.prototype.draw = function() {
-    var i;
+    let i;
+    // Loop through each block in the group and call the draw method on it
     for (i = 0; i < 4; i += 1) {
-	this.blocks[i].draw();
+        this.blocks[i].draw();
     }
 };
-				
