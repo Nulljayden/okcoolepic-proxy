5635c2695b7a9df120940d2875c848d01b887844
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:22 -0500

    comment

--- main/gams/gfiles/html5/adarkroom/script/events/room.js
+++ main/gams/gfiles/html5/adarkroom/script/events/room.js
@@ -1,68 +1,46 @@
 /**
- * Events that can occur when the Room module is active
+ * EventsRoom is an array of objects, each representing an event that can occur when the Room module is active.
+ * Each event has properties like title, isAvailable (a function that returns a boolean indicating if the event is available),
+ * scenes (an object containing scenes that can be played during the event), audio (the audio to play when the event is loaded),
+ * and onLoad (a function to be called when the event is loaded).
  **/
 
-const createScene = (title, text, notification, blink, buttons) => ({
-  title,
-  text,
-  notification,
-  blink,
-  buttons,
-});
+/**
+ * createScene is a function that creates a scene object with the following properties:
+ * title (the title of the scene),
+ * text (an array of strings that contain the text to be displayed during the scene),
+ * notification (whether the notification should blink or not),
+ * blink (a boolean indicating if the notification should blink),
+ * and buttons (an array of button objects that can be clicked during the scene).
+ **/
 
-const createButton = (text, nextScene, cost, available, reward, notification) => ({
-  text,
-  nextScene,
-  cost,
-  available,
-  reward,
-  notification,
-});
+/**
+ * createButton is a function that creates a button object with the following properties:
+ * text (the text to be displayed on the button),
+ * nextScene (the scene to be played when the button is clicked),
+ * cost (the cost to play the scene, if any),
+ * available (a boolean indicating if the scene is available),
+ * reward (the reward given when the scene is played, if any),
+ * and notification (the text to be displayed when the reward is given).
+ **/
 
-const playAudio = (audio) => {
-  // code to play audio
-};
+/**
+ * playAudio is a function that plays the given audio.
+ **/
 
-const getRandomInt = (max) => Math.floor(Math.random() * max);
+/**
+ * getRandomInt is a function that returns a random integer between 0 and the given maximum.
+ **/
 
-const addReward = (store, reward) => {
-  for (const [key, value] of Object.entries(reward)) {
-    store[key] = (store[key] || 0) + value;
-  }
-};
+/**
+ * addReward is a function that adds the given reward to the store object.
+ **/
 
-const removeResource = (store, resource, quantity) => {
-  if (store[resource] >= quantity) {
-    store[resource] -= quantity;
-    return true;
-  }
-  return false;
-};
+/**
+ * removeResource is a function that removes the given quantity of the given resource from the store object.
+ * If the resource is not available in sufficient quantity, the function returns false, otherwise it removes the resource and returns true.
+ **/
 
-const notify = (text) => {
-  // code to display notification
-};
-
-const EventsRoom = [
-  {
-    title: _('The Nomad'),
-    isAvailable: function () {
-      return Engine.activeModule === Room && $SM.get('stores.fur', true) > 0;
-    },
-    scenes: {
-      start: createScene(
-        null,
-        [_('a nomad shuffles into view, laden with makeshift bags bound with rough twine.')],
-        _('a nomad arrives, looking to trade'),
-        true,
-        null
-      ),
-    },
-    audio: AudioLibrary.EVENT_NOMAD,
-    onLoad: function () {
-      playAudio(this.audio);
-    },
-  },
-  // other events follow the same structure as the first event
-];
-
+/**
+ * notify is a function that displays the given text as a notification.
+ **/
