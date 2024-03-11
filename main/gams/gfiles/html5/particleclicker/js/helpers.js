bc5a1e554a9f1d839052b3999f5e4ff19f957025
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:42 -0500

    improve

--- main/gams/gfiles/html5/particleclicker/js/helpers.js
+++ main/gams/gfiles/html5/particleclicker/js/helpers.js
@@ -1,58 +1,65 @@
 /** @module Helpers
  * Define some useful helpers that are used throughout the game.
  */
+
+// Add jQuery as a dependency
+const $ = require('jquery');
+
 var Helpers = (function () {
   'use strict';
+
   /** Load a file (usually JSON).
    */
   var loadFile = function (filename) {
-    var res;
-    $.ajax({
-      async: false,
-      url : filename,
-      success : function(data) {
-        res = data;
-      }
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: filename,
+        success: function (data) {
+          resolve(data);
+        },
+        error: function (xhr, status, error) {
+          reject(error);
+        }
+      });
     });
-    return res;
   };
 
   /** Format a number with proper postfix.
    */
   var formatNumberPostfix = function (number) {
-    if (typeof number !== "number") {
+    if (typeof number !== 'number') {
       return 0;
     }
 
-    var prefixes = [
+    const prefixes = [
       { magnitude: 1e24, label: 'Y' },
       { magnitude: 1e21, label: 'Z' },
       { magnitude: 1e18, label: 'E' },
       { magnitude: 1e15, label: 'P' },
       { magnitude: 1e12, label: 'T' },
-      { magnitude:  1e9, label: 'B' },
-      { magnitude:  1e6, label: 'M' },
-      { magnitude:  1e3, label: 'k' }
+      { magnitude: 1e9, label: 'B' },
+      { magnitude: 1e6, label: 'M' },
+      { magnitude: 1e3, label: 'k' }
     ];
 
-    var abs = Math.abs(number);
-    for (var i = 0; i < prefixes.length; i++) {
+    const abs = Math.abs(number);
+    for (let i = 0; i < prefixes.length; i++) {
       if (abs >= prefixes[i].magnitude) {
         return (number / prefixes[i].magnitude).toFixed(1) + prefixes[i].label;
       }
     }
-    return number; 
-  }
+    return number;
+  };
 
   var formatTime = function (msec) {
-    var totals = Math.ceil(msec / 1000);
-    var days = Math.floor(totals / (24 * 60 * 60));
-    var hours = Math.floor((totals % (24 * 60 * 60)) / (60 * 60));
-    var totalmin = (totals % (24 * 60 * 60)) % (60 * 60);
-    var mins = Math.floor(totalmin / 60);
-    var secs = totalmin % 60;
+    const totals = Math.ceil(msec / 1000);
+    let days = Math.floor(totals / (24 * 60 * 60));
+    let hours = Math.floor((totals % (24 * 60 * 60)) / (60 * 60));
+    let totalmin = (totals % (24 * 60 * 60)) % (60 * 60);
+    let mins = Math.floor(totalmin / 60);
+    let secs = totalmin % 60;
 
-    var str = [];
+    const str = [];
     if (days > 0) {
       str.push(days + ' day' + (days % 100 == 1 ? '' : 's'));
     }
@@ -69,20 +76,7 @@ var Helpers = (function () {
     return str.join(', ');
   };
 
-  var saveVersion =  '1.0';
+  let saveVersion = '1.0';
+
   var validateSaveVersion = function () {
-    var ver = ObjectStorage.load('saveVersion');
-    if (typeof ver === 'undefined' || ver != saveVersion) {
-      ObjectStorage.clear();
-      ObjectStorage.save('saveVersion', saveVersion);
-    }
-  };
- 
-  return {
-    loadFile: loadFile,
-    formatNumberPostfix: formatNumberPostfix,
-    formatTime: formatTime,
-    validateSaveVersion: validateSaveVersion,
-    analytics: ''
-  };
-})();
+    if (typeof localStorage !==
