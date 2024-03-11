fefdfddc8f1c0e809ab8ea4a47542408677d5699
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:21 -0500

    comment

--- main/gams/gfiles/html5/blackholesquare/sw.js
+++ main/gams/gfiles/html5/blackholesquare/sw.js
@@ -1,1 +1,45 @@
-(()=>{let e="black-hole-square",t=["index.html","icon.png","manifest.json"];self.addEventListener("install",(n=>{n.waitUntil(caches.open(e).then((e=>e.addAll(t))).then(self.skipWaiting()))})),self.addEventListener("activate",(t=>{const n=[e];t.waitUntil(caches.keys().then((e=>e.filter((e=>!n.includes(e))))).then((e=>Promise.all(e.map((e=>caches.delete(e)))))).then((()=>self.clients.claim())))})),self.addEventListener("fetch",(t=>{t.respondWith(fetch((e=>{let t=new Request(e,{cache:"reload"});if("cache"in t)return t;let n=new URL(e,self.location.href);return n.search+=(n.search?"&":"")+"cachebust="+Date.now(),new Request(n)})(t.request.url)).then((n=>(caches.open(e).then((e=>e.put(t.request.url,n.clone()))),n.clone()))).catch((e=>caches.match(t.request.url))))}))})();
+// This script is a service worker that uses the Cache API to cache static assets
+// of a web application for offline access.
+
+(() => {
+  // Define the cache name as 'black-hole-square'.
+  const cacheName = 'black-hole-square';
+
+  // Define the list of static assets to be cached.
+  const cacheAssets = ['index.html', 'icon.png', 'manifest.json'];
+
+  // Event listener for the 'install' event.
+  self.addEventListener('install', (event) => {
+    // Wait until the cache is opened and add all the assets to the cache.
+    event.waitUntil(
+      caches.open(cacheName)
+        .then((cache) => cache.addAll(cacheAssets))
+        .then(() => self.skipWaiting())
+    );
+  });
+
+  // Event listener for the 'activate' event.
+  self.addEventListener('activate', (event) => {
+    // Define the list of caches to be deleted.
+    const cachesToDelete = [cacheName];
+
+    // Wait until all the caches are deleted and claim the clients.
+    event.waitUntil(
+      caches.keys()
+        .then((keys) => keys.filter((key) => !cachesToDelete.includes(key)))
+        .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+        .then(() => self.clients.claim())
+    );
+  });
+
+  // Event listener for the 'fetch' event.
+  self.addEventListener('fetch', (event) => {
+    // Define the request with the 'reload' cache mode.
+    const request = new Request(event.request, { cache: 'reload' });
+
+    // If the 'cache' property is in the request, return the request.
+    if ('cache' in request) return event.respondWith(fetch(request));
+
+    // Create a new URL with the cache-busting parameter.
+    const url = new URL(event.request.url, self.location.href);
+    url.search += (url.search ? '&' : '')
