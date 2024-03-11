dc3b57bf37c339bb605267f8474c6b0df3b42c24
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:12 -0500

    comment

--- main/gams/gfiles/html5/spacecompany/ui/achievementUI.js
+++ main/gams/gfiles/html5/spacecompany/ui/achievementUI.js
@@ -1,167 +1,135 @@
-Game.achievementsUI = (function(){
+// Create a module pattern for Game.achievementsUI
+Game.achievementsUI = (function() {
 
-    var instance = {};
+  // Create an instance object to hold all the methods and properties
+  var instance = {};
 
-    instance.categoryTemplate = null;
-    instance.entryTemplate = null;
+  // Define templates for handlingbars
+  instance.rankTemplate = null;
+  instance.entryTemplate = null;
+  instance.categoryTemplate = null;
 
-    instance.categoryElements = {};
-    instance.rootElement = null;
+  // Initialize variables to hold DOM elements
+  instance.categoryElements = {};
+  instance.rootElement = null;
 
-    instance.initialise = function() {
-        this.rankTemplate = Handlebars.compile(
-            ['<td>',
-                '<h3 class="default btn-link">Current Rank: <br><span id="rankNumber">{{number}}</span> - <span id="rankName">{{name}}</span></h3>',
-                '</td>'].join('\n'));
+  // Initialize the instance
+  instance.initialise = function() {
+    // Compile the rank template
+    instance.rankTemplate = Handlebars.compile(
+      ['<td>',
+        '<h3 class="default btn-link">Current Rank: <br><span id="rankNumber">{{number}}</span> - <span id="rankName">{{name}}</span></h3>',
+        '</td>'].join('\n'));
 
-        this.rankBarTemplate = Handlebars.compile(
-            ['<td colspan="2">',
-                '<div id="rankProgress" class="progress">',
-                    '<div id="rankBar" class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">',
-                        '0%',
-                    '</div>',
-                '</div>',
-                '</td>'].join('\n'));
+    // Compile the rank bar template
+    instance.rankBarTemplate = Handlebars.compile(
+      ['<td colspan="2">',
+        '<div id="rankProgress" class="progress">',
+          '<div id="rankBar" class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">',
+            '0%',
+          '</div>',
+        '</div>',
+        '</td>'].join('\n'));
 
-        this.categoryTemplate = Handlebars.compile(
-            ['<td>',
-                '<h3 class="default btn-link">{{name}} (<span id="{{id}}_unlocked">0</span>/<span id="{{id}}_total">0</span>)</h3>',
-                '<table class="table" id="{{id}}"></table>',
-                '</td>'].join('\n'));
+    // Compile the category template
+    instance.categoryTemplate = Handlebars.compile(
+      ['<td>',
+        '<h3 class="default btn-link">{{name}} (<span id="{{id}}_unlocked">0</span>/<span id="{{id}}_total">0</span>)</h3>',
+        '<table class="table" id="{{id}}"></table>',
+        '</td>'].join('\n'));
 
-        this.entryTemplate = Handlebars.compile(
-            ['<td id="{{id}}" class="achievementTD" style="border:none;">',
-                '<div id="{{id}}_div" data-toggle="tooltip" title="{{title}}" style="width: 64px; height: 64px; border:2px solid white;">',
-                '<div id="{{id}}_bg" style="width: 50px; height: 40px; background: url({{iconPath}}{{iconName}}.{{iconExtension}}) no-repeat center; -webkit-background-size: contain;background-size: contain; margin-left: 5px;opacity: 0.2"></div>',
-                '<div id="{{id}}_img" style="overflow: hidden; vertical-align: bottom;"><img src="Icons/achievementStar.png" height="11px"></div>',
-                '</div>',
-                '</td>'].join('\n'));
+    // Compile the entry template
+    instance.entryTemplate = Handlebars.compile(
+      ['<td id="{{id}}" class="achievementTD" style="border:none;">',
+        '<div id="{{id}}_div" data-toggle="tooltip" title="{{title}}" style="width: 64px; height: 64px; border:2px solid white;">',
+        '<div id="{{id}}_bg" style="width: 50px; height: 40px; background: url({{iconPath}}{{iconName}}.{{iconExtension}}) no-repeat center; -webkit-background-size: contain;background-size: contain; margin-left: 5px;opacity: 0.2"></div>',
+        '<div id="{{id}}_img" style="overflow: hidden; vertical-align: bottom;"><img src="Icons/achievementStar.png" height="11px"></div>',
+        '</div>',
+        '</td>'].join('\n'));
 
-        this.rootElement = $('#achievementContent');
+    // Set the root element to the achievement content
+    instance.rootElement = $('#achievementContent');
 
-        this.createRank(0);
+    // Create the first rank
+    instance.createRank(0);
 
-        for(var id in Game.achievements.entries) {
-            this.createDisplay(id);
-        }
-    };
+    // Loop through all the achievements and create their displays
+    for (var id in Game.achievements.entries) {
+      instance.createDisplay(id);
+    }
+  };
 
-    instance.update = function(delta) {
-        var categoryCounts = {};
-        var updateCategories = false;
-        var totalAchieved = 0;
+  // Update the achievements UI
+  instance.update = function(delta) {
+    var categoryCounts = {};
+    var updateCategories = false;
+    var totalAchieved = 0;
 
+    // Initialize categoryCounts
+    for (var category in instance.categoryElements) {
+      categoryCounts[category] = {
+        unlocked: 0,
+        total: 0
+      };
+    }
 
-        for(var category in this.categoryElements) {
-          categoryCounts[category] = { unlocked: 0, total: 0 };
-        }
-        
-        for(var id in Game.achievements.entries) {
-            var data = Game.achievements.entries[id];
-            
-            totalAchieved += Game.utils.pascal(data.unlocked+1);
-            categoryCounts[data.category].unlocked += data.unlocked + 1;
-            categoryCounts[data.category].total += data.brackets.length;
+    // Loop through all the achievements and update the counts
+    for (var id in Game.achievements.entries) {
+      var data = Game.achievements.entries[id];
 
-            if(data.displayNeedsUpdate === true) {
-                this.updateDisplay(id);
-                updateCategories = true;
-            }
-        }
-        
-        if (updateCategories === true) {
-          for(var category in this.categoryElements) {
-            var id = this.categoryElements[category].id;
-            $('#' + id + '_unlocked').text(categoryCounts[category].unlocked);
-            $('#' + id + '_total').text(categoryCounts[category].total);
-            
-          }
-        }
+      // Update the total achieved and category counts
+      totalAchieved += Game.utils.pascal(data.unlocked + 1);
+      categoryCounts[data.category].unlocked += data.unlocked + 1;
+      categoryCounts[data.category].total += data.brackets.length;
 
-        // Calculating Ranks
+      // Check if the display needs to be updated
+      if (data.displayNeedsUpdate === true) {
+        instance.updateDisplay(id);
+        updateCategories = true;
+      }
+    }
 
-        var x = Game.achievements.rank;
-        var xpNeeded = Game.utils.fibonacci(x+7);
-        var xpLeft = Game.utils.fibonacci(x+7) - totalAchieved;
-        if(xpLeft <= 0){
-            Game.achievements.rank += 1;
-        }
-        var percentLeft = Game.settings.format(100-(xpLeft/xpNeeded*100)) + '%';
-        $('#rankBar').text(percentLeft + ' (' + (xpNeeded-xpLeft) + '/' + xpNeeded + ')');
-        $('#rankBar').width(percentLeft);
-        $('#rankNumber').text(x);
-        $('#rankName').text(Game.constants.rank[x]);
+    // Update the categories if any displays were updated
+    if (updateCategories === true) {
+      for (var category in instance.categoryElements) {
+        var id = instance.categoryElements[category].id;
+        $('#' + id + '_unlocked').text(categoryCounts[category].unlocked);
+        $('#' + id + '_total').text(categoryCounts[category].total);
+      }
+    }
 
-        
-    };
+    // Calculate the ranks
 
-    instance.createDisplay = function(id) {
-        var data = Game.achievements.entries[id];
+    // Get the current rank and experience needed
+    var xpNeeded = Game.utils.fibonacci(instance.currentRank + 7);
+    var xpLeft = Game.utils.fibonacci(instance.currentRank + 7) - totalAchieved;
 
-        if (this.categoryElements[data.category] === undefined) {
-            this.createCategory(data.category);
-        }
+    // If the player has enough experience, increase the rank
+    if (xpLeft <= 0) {
+      instance.currentRank += 1;
+    }
 
-        var html = this.entryTemplate(data);
-        this.categoryElements[data.category].colc++;
-        this.categoryElements[data.category].col.append($(html)).find('[data-toggle="tooltip"]').tooltip();
+    // Calculate the percentage of experience left
+    var percentLeft = Game.settings.format(100 - (xpLeft / xpNeeded * 100)) + '%';
 
-        if(this.categoryElements[data.category].colc >= Game.constants.achievementIconsPerRow) {
-            this.createCategoryRow(data.category);
-        }
-    };
+    // Update the rank bar and number
+    $('#rankBar').text(percentLeft + ' (' + (xpNeeded - xpLeft) + '/' + xpNeeded + ')');
+    $('#rankBar').width(percentLeft);
+    $('#rankNumber').text(instance.currentRank);
+    $('#rankName').text(Game.constants.rank[instance.currentRank]);
 
-    instance.updateDisplay = function(id) {
-        var data = Game.achievements.entries[id];
-        var div = $('#' + id + "_div");
-        var bg = $('#' + id + "_bg");
+  };
 
-        div.attr('data-original-title', Game.achievements.getAchievementTitle(data, true));
+  // Create a display for an achievement
+  instance.createDisplay = function(id) {
+    var data = Game.achievements.entries[id];
 
-        div.css('border-color', Game.constants.achievementBracketColors[data.unlocked]);
-        if(data.unlocked == -1){
-            div.css('border-color', '#ffffff');
-            //div.fadeTo(2, 0.2);
-        }
+    // If the category doesn't exist, create it
+    if (instance.categoryElements[data.category] === undefined) {
+      instance.createCategory(data.category);
+    }
 
-        $('#' + id + '_img').width(12 * (data.unlocked + 1));
+    // Create the HTML for the achievement
+    var html = instance.entryTemplate(data);
 
-        if(data.unlocked >= 0){
-            bg.fadeTo(2, 1);
-        }
-
-        data.displayNeedsUpdate = false;
-    };
-
-    instance.createCategory = function (category) {
-        var data = {id: "ach_cat_" + category, name: category};
-        this.categoryElements[category] = {colc: 0, col: null, id: data.id};
-
-        var html = $(this.categoryTemplate(data));
-        this.rootElement.append(html);
-
-        this.createCategoryRow(category);
-    };
-
-    instance.createCategoryRow = function (category) {
-        var data = this.categoryElements[category];
-
-        data.colc = 0;
-        data.col = $('<tr></tr>');
-        $('#'+data.id).append(data.col);
-    };
-
-    instance.createRank = function (x) {
-        var data = {name: Game.constants.rank[x], number: x};
-        var html = this.rankTemplate(data);
-        $('#rankContent').append(html);
-
-        var html = this.rankBarTemplate();
-        $('#rankBarContent').append(html);
-    };
-
-    Game.uiComponents.push(instance);
-
-    return instance;
-
-}());
+    // Increment the column
