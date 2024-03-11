079f4fcb9a7906b2c12e428e936faab93b5bb2d5
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:23 -0500

    improve

--- main/js/gams.js
+++ main/js/gams.js
@@ -1,3 +1,17 @@
-if(location.hash != ''){
-    document.querySelector(".gam").src = window.location.origin + location.hash.substring(1);
+const hash = location.hash;
+if (hash) {
+  const baseUrl = window.location.origin;
+  const hashPath = hash.substring(1);
+  const validPath = /^[a-zA-Z0-9/._-]+$/.test(hashPath);
+
+  if (validPath) {
+    const element = document.querySelector('.gam');
+    if (element) {
+      element.src = `${baseUrl}${hashPath}`;
+    } else {
+      console.warn('.gam element not found');
+    }
+  } else {
+    console.error('Invalid hash path:', hashPath);
+  }
 }
