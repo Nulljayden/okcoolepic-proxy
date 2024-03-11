a20bdfcf39a5f461f358fd1659c3bc5445b08c3c
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:59 -0500

    improve

--- main/gams/gfiles/html5/tetris/cookie.js
+++ main/gams/gfiles/html5/tetris/cookie.js
@@ -1,24 +1,45 @@
-function createCookie(name,value,days) {
-    if (days) {
-	var date = new Date();
-	date.setTime(date.getTime()+(days*24*60*60*1000));
-	var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
+type CookieOptions = {
+  days?: number;
+};
+
+/**
+ * Create a cookie with the given name, value, and optional days until expiration.
+ * @param name - The name of the cookie.
+ * @param value - The value of the cookie.
+ * @param options - Optional settings for the cookie.
+ */
+function createCookie(name: string, value: string, options: CookieOptions = {}): void {
+  const { days } = options;
+  const expirationDate = days
+    ? new Date(Date.now() + days * 24 * 60 * 60 * 1000)
+    : undefined;
+  const expires = expirationDate ? `expires=${expirationDate.toUTCString()};` : '';
+  document.cookie = `${name}=${value}; ${expires} path=/`;
 }
 
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-	var c = ca[i];
-	while (c.charAt(0)==' ') c = c.substring(1,c.length);
-	if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+/**
+ * Read the value of a cookie with the given name.
+ * @param name - The name of the cookie.
+ * @returns The value of the cookie or null if not found.
+ */
+function readCookie(name: string): string | null {
+  const nameEQ = `${name}=`;
+  const cookies: string[] = document.cookie.split(';');
+
+  for (const cookie of cookies) {
+    let c = cookie.trim();
+    if (c.startsWith(nameEQ)) {
+      return c.substring(nameEQ.length);
     }
-    return null;
+  }
+
+  return null;
 }
 
-function eraseCookie(name) {
-    createCookie(name,"",-1);
+/**
+ * Erase a cookie with the given name.
+ * @param name - The name of the cookie.
+ */
+function eraseCookie(name: string): void {
+  createCookie(name, '', { days: -1 });
 }
