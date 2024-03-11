2662325632accc2fd761513764ccf8bf657690e8
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:47 -0500

    improve

--- main/gams/gfiles/html5/radiusraid/js/particleemitter.js
+++ main/gams/gfiles/html5/radiusraid/js/particleemitter.js
@@ -1,43 +1,51 @@
 /*==============================================================================
-Init
+Particle Emitter
 ==============================================================================*/
-$.ParticleEmitter = function( opt ) {
-	for( var k in opt ) {
-		this[k] = opt[k];
-	}
-	this.particles = [];
-	for( var i = 0; i < this.count; i++ ) {
-		var radius = Math.sqrt( Math.random() ) * this.spawnRange,
-            angle = Math.random() * $.twopi,
-            x = this.x + Math.cos( angle ) * radius,
-            y = this.y + Math.sin( angle ) * radius;
-		this.particles.push( new $.Particle( {
-			parent: this.particles,
-			x: x,
-			y: y,
-			speed: $.util.rand( this.minSpeed, this.maxSpeed ),
-			friction: this.friction,
-			direction: $.util.rand( this.minDirection, this.maxDirection ),
-			lineWidth: $.util.rand( 0.5, 1.5 ),
-			hue: this.hue,
-			saturation: this.saturation
-		} ) );
-	}
+
+// Check if an object is an array
+function isArray(obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+}
+
+// Particle constructor
+function Particle(options) {
+  if (options.parent && !isArray(options.parent)) {
+    throw new Error('options.parent must be an array');
+  }
+
+  if (!options.x || !options.y || !options.speed || !options.friction || !options.direction || !options.lineWidth || !options.hue || !options.saturation) {
+    throw new Error('options must contain x, y, speed, friction, direction, lineWidth, hue, and saturation properties');
+  }
+
+  this.parent = options.parent || [];
+  this.x = options.x;
+  this.y = options.y;
+  this.speed = options.speed;
+  this.friction = options.friction;
+  this.direction = options.direction;
+  this.lineWidth = options.lineWidth;
+  this.hue = options.hue;
+  this.saturation = options.saturation;
+}
+
+// Remove a particle from its parent array
+Particle.prototype.remove = function() {
+  var index = this.parent.indexOf(this);
+  if (index !== -1) {
+    this.parent.splice(index, 1);
+  }
 };
 
-/*==============================================================================
-Update
-==============================================================================*/
-$.ParticleEmitter.prototype.update = function( i ) {
-	var i2 = this.particles.length; while( i2-- ){ this.particles[ i2 ].update( i2 ) }
-	if( this.particles.length <= 0 ) {
-		$.particleEmitters.splice( i, 1 );
-	}
-};
+// Particle Emitter constructor
+function ParticleEmitter(opt) {
+  if (!isArray(opt)) {
+    throw new Error('opt must be an array');
+  }
 
-/*==============================================================================
-Render
-==============================================================================*/
-$.ParticleEmitter.prototype.render = function( i ) {
-	var i2 = this.particles.length; while( i2-- ){ this.particles[ i2 ].render( i2 ) }
-};
+  for (var k in opt) {
+    this[k] = opt[k];
+  }
+
+  this.particles = [];
+
+  for (var i = 0; i < this.count
