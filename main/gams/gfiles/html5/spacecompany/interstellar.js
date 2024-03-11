758709c51730a9ddc2668b8ca1e3fc75a388000b
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:06 -0500

    comment

--- main/gams/gfiles/html5/spacecompany/interstellar.js
+++ main/gams/gfiles/html5/spacecompany/interstellar.js
@@ -1,41 +1,46 @@
-Game.interstellar = (function () {
-    let _navCount = 0;
+// The Game.interstellar module, which is an IIFE (Immediately Invoked Function Expression)
+Game.interstellar = (function() {
+    let _navCount = 0; // A private variable to keep track of the number of entries
 
+    // An object that contains all the data and methods related to interstellar communications
     const instance = {
-        dataVersion: 1,
-        entries: {},
-        comms: (function () {
-            const entries = {};
+        dataVersion: 1, // The version of the data format
+        entries: {}, // An object to store all the entries
+        comms: (function() {
+            const entries = {}; // An object to store the communication entries
 
+            // A function to build a communication machine
             function buildMachine(id) {
-                const data = getMachineById(id);
+                const data = getMachineById(id); // Get the data of the machine
                 if (data.count >= data.max) {
-                    return;
+                    return; // If the machine has reached its maximum capacity, do nothing
                 }
-                let resourcePass = 0;
-                for (const resource of Object.values(data.cost)) {
+                let resourcePass = 0; // Initialize a variable to track the number of valid resources
+                for (const resource of Object.values(data.cost)) { // Iterate over the cost of the machine
                     if (Number.isFinite(window[resource.toString()]) && window[resource.toString()] >= resource) {
-                        resourcePass += 1;
+                        resourcePass += 1; // If the resource is valid and sufficient, increment the resourcePass counter
                     }
                 }
-                if (resourcePass === Object.values(data.cost).length) {
-                    data.count += 1;
+                if (resourcePass === Object.values(data.cost).length) { // If all required resources are valid and sufficient
+                    data.count += 1; // Build the machine
                     for (const resource in data.cost) {
-                        window[resource.toString()] -= data.cost[resource.toString()];
+                        window[resource.toString()] -= data.cost[resource.toString()]; // Deduct the cost of the machine
                     }
-                    data.displayNeedsUpdate = true;
+                    data.displayNeedsUpdate = true; // Update the display
                 }
             }
 
+            // A function to get the data of a communication machine by its id
             function getMachineDataById(id) {
                 return entries[id];
             }
 
+            // A function to initialize the communication machines
             function initialise() {
-                for (const id in Game.commsData) {
-                    const data = Game.commsData[id];
-                    _navCount++;
-                    entries[id] = clone(data, {
+                for (const id in Game.commsData) { // Iterate over the Game.commsData
+                    const data = Game.commsData[id]; // Get the data of a communication machine
+                    _navCount++; // Increment the _navCount
+                    entries[id] = clone(data, { // Clone the data and add some extra properties
                         id: id,
                         htmlId: 'comm_' + id,
                         count: 0,
@@ -44,6 +49,7 @@ Game.interstellar = (function () {
                 }
             }
 
+            // Return an object containing all the methods and data of the communication module
             return {
                 entries,
                 initialise,
@@ -51,7 +57,8 @@ Game.interstellar = (function () {
                 getMachineDataById
             };
         })(),
-        antimatter: (function () {
+        // The same structure is repeated for antimatter and military modules
+        antimatter: (function() {
             const entries = {};
 
             function buildMachine(id) {
@@ -82,7 +89,7 @@ Game.interstellar = (function () {
                 getMachineDataById
             };
         })(),
-        military: (function () {
+        military: (function() {
             const entries = {};
 
             function buildMachine(id) {
@@ -104,4 +111,15 @@ Game.interstellar = (function () {
                         active: 0,
                         displayNeedsUpdate: true
                     });
-              
+                }
+            }
+
+            return {
+                entries,
+                initialise,
+                buildMachine,
+                getMachineDataById
+            };
+        })()
+    };
+})();
