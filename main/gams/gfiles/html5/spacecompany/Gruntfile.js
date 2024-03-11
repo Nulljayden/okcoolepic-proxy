86a16eb9b8e32694bc72cb7d060c826e358228b0
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:59 -0500

    comment

--- main/gams/gfiles/html5/spacecompany/Gruntfile.js
+++ main/gams/gfiles/html5/spacecompany/Gruntfile.js
@@ -5,29 +5,26 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    pkg: grunt.file.readJSON('package.json'), // Read the package.json file and parse it as a JSON object.
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Authored <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;*/\n',
+      '* Authored <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;*/\n', // Define a banner comment for the top of the compiled file.
 
     // Task configuration.
     concat: {
       options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+        banner: '<%= banner %>', // Use the banner defined above.
+        stripBanners: true // Remove all banner comments from the concatenated files.
       },
       dist: {
-        src: ['variable.js', 'utils.js', 'game.js', 'achievements.js', 'constants.js', 'core.js', 'loading.js', 'notification.js', 'resources.js', 'saving.js', 'solarSystem.js', 'solCenter.js', 'wonder.js', 'interstellar.js'],
-        dest: 'build/<%= pkg.name %>.js'
+        src: ['variable.js', 'utils.js', 'game.js', 'achievements.js', 'constants.js', 'core.js', 'loading.js', 'notification.js', 'resources.js', 'saving.js', 'solarSystem.js', 'solCenter.js', 'wonder.js', 'interstellar.js'], // List of files to concatenate.
+        dest: 'build/<%= pkg.name %>.js' // Destination file for the concatenated files.
       }
     },
     uglify: {
       options: {
-        banner: '<%= banner %>'
+        banner: '<%= banner %>' // Use the banner defined above.
       },
       dist: {
-        src: 'build/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-
+        src: 'build/<%= pkg.name %>.js', // File to minify.
+        dest: 'build/<%= pkg.name %>.min.js' // Destination file for the min
