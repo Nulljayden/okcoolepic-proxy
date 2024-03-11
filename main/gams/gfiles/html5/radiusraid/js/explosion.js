ae210a1436ae4c90fd23a7c41ae6d268c9f721a7
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:37 -0500

    comment

--- main/gams/gfiles/html5/radiusraid/js/explosion.js
+++ main/gams/gfiles/html5/radiusraid/js/explosion.js
@@ -1,34 +1,3 @@
 /*==============================================================================
-Explosion
-==============================================================================*/
-$.Explosion = function(options) {
-  for (var key in options) {
-    if (options.hasOwnProperty(key)) {
-      this[key] = options[key];
-    }
-  }
-
-  this.tick = 0;
-  this.tickMax = 20;
-
-  if ($.slow) {
-    $.audio.play('explosionAlt');
-  } else {
-    $.audio.play('explosion');
-  }
-};
-
-/*==============================================================================
-Update
-==============================================================================*/
-$.Explosion.prototype.update = function(index) {
-  if (this.tick >= this.tickMax) {
-    $.explosions.splice(index, 1);
-  } else {
-    this.tick += $.dt;
-  }
-};
-
-/*==============================================================================
-Render
-================================
+Explosion Constructor
+================================================
