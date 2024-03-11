eeb215c73d55ab792818866e0b1dfef33e93584c
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:41 -0500

    comment

--- main/gams/gfiles/html5/radiusraid/js/textpop.js
+++ main/gams/gfiles/html5/radiusraid/js/textpop.js
@@ -1,13 +1,14 @@
 /**
  * TextPop object constructor
  * @param {object} opt - object containing the object properties
+ * The constructor for the TextPop object, which takes in an object 'opt'
+ * containing the object properties.
  */
 function TextPop(opt) {
   // Set the object properties
   for (var k in opt) {
     this[k] = opt[k];
   }
-
   // Set default values for the object properties
   this.alpha = 2;
   this.vy = 0;
@@ -33,6 +34,9 @@ function TextPop(opt) {
 /**
  * Update the object properties
  * @param {number} i - index of the object in the array
+ * The method updates the object properties, including the vertical velocity 'vy',
+ * the y-coordinate, and the alpha value (transparency). If the alpha value is less
+ * than or equal to 0, the object is removed from the array.
  */
 TextPop.prototype.update = function(i) {
   this.vy -= 0.05 * $.dt;
@@ -40,4 +44,7 @@ TextPop.prototype.update = function(i) {
   this.alpha -= 0.03 * $.dt;
 
   // Remove the object from the array if its alpha value is less than or equal to 0
-  this.delete
+  if (this.alpha <= 0) {
+    $.TextPops.splice(i, 1);
+  }
+};
