ae83bff9c17798468c92605cee118fd5a7853369
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:21 -0500

    improve

--- main/uv/uv.index.js
+++ main/uv/uv.index.js
@@ -1,1 +1,38 @@
-function decodeUrl(str){if (!str) return str;let [ input, ...search ] = str.split('?');return decodeURIComponent(input).split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char).join('') + (search.length ? '?' + search.join('?') : '');}window.onload = function(){var i = document.createElement('script');i.type = 'text/javascript';i.text = "if(new URL(decodeUrl(/[^/]*$/.exec(window.location.pathname)[0])).hostname.replace('www.','') == 'youtube.com'){var v = document.querySelector('#movie_player');function s () {if(v.classList.contains('ad-showing')){document.querySelector('video').currentTime = document.querySelector('video').duration;}}s();if(!observer){var observer = new MutationObserver(s);observer.observe(v, {attributes: true,attributeFilter: ['class'],childList: false, characterData: false});}}";document.body.appendChild(i);}
+function decodeUrl(str) {
+  if (!str) return str;
+  const [input, ...search] = str.split('?');
+  return decodeURIComponent(input)
+    .split('')
+    .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char))
+    .join('') + (search.length ? '?' + search.join('?') : '');
+}
+
+window.onload = function () {
+  const i = document.createElement('script');
+  i.type = 'text/javascript';
+
+  const url = new URL(decodeUrl(/[^/]*$/.exec(window.location.pathname)[0]));
+  if (url.hostname.replace('www.', '') === 'youtube.com') {
+    const videoPlayer = document.querySelector('#movie_player');
+
+    function skipAd() {
+      if (videoPlayer.classList.contains('ad-showing')) {
+        videoPlayer.currentTime = videoPlayer.duration;
+      }
+    }
+
+    skipAd();
+
+    const observer = new MutationObserver(skipAd);
+    observer.observe(videoPlayer, {
+      attributes: true,
+      attributeFilter: ['class'],
+      childList: false,
+      characterData: false,
+    });
+
+    i.text = `(${skipAd.toString()})()`;
+  }
+
+  document.body.appendChild(i);
+};
