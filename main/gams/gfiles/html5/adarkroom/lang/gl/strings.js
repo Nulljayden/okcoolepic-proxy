8ff591e5725df8b9908d88ff7d9a1961bbc4049c
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:29 -0500

    comment

--- main/gams/gfiles/html5/adarkroom/lang/gl/strings.js
+++ main/gams/gfiles/html5/adarkroom/lang/gl/strings.js
@@ -1,70 +1,54 @@
+// An object that stores translations for various phrases and words.
 const translations = {
-  "expanded story. alternate ending. behind the scenes commentary. get the app.":
+  // The key is a phrase or word in English, and the value is its translation in Galician.
+  "expanded story. alternate ending. behind the scenes commentary. get the app.": 
     "historia expandida. final alternativo. comentarios behind the scenes. faite coa app.",
   "water tank": "tanque de auga",
   "all": "todo",
   "use meds": "usar menci\u00f1as",
-  "a shame to let what he'd found go to waste.":
+  "a shame to let what he'd found go to waste.": 
     "ser\u00eda unha m\u00e1goa desaproveitar todo o que atopou.",
   "some villagers are ill": "alg\u00fans alde\u00e1ns enfermaron",
   "the room is {0}": "o cuarto est\u00e1 {0}",
-  "punch twice as fast, and with even more force":
+  "punch twice as fast, and with even more force": 
     "golpea o dobre de r\u00e1pido, e con m\u00e1is forza a\u00ednda",
   "The Nomad": "O N\u00f3mada",
   "more traps won't help now": "m\u00e1is trampas non sirven de nada agora",
   "only a few die.": "s\u00f3 morren uns poucos.",
   "the compass points east": "o comp\u00e1s apunta ao leste",
-  "the bodies of the wanderers that lived here are still visible in the flames.":
+  "the bodies of the wanderers that lived here are still visible in the flames.": 
     "os corpos dos errantes que viviron aqu\u00ed a\u00ednda se distinguen entre as chamas.",
-  "the walls are scorched from an old battle.":
+  "the walls are scorched from an old battle.": 
     "os muros est\u00e1n chamuscados dunha antiga batalla.",
   "convoy": "convoi",
   "not enough fur": "non hai pel abonda",
-  "a masked soldier rounds the corner, gun drawn":
+  "a masked soldier rounds the corner, gun drawn": 
     "un soldado enmascarado dobra a esquina, coa pistola na man",
-  "a huge hole is cut deep into the earth, evidence of the past harvest.":
+  "a huge hole is cut deep into the earth, evidence of the past harvest.": 
     "un burato enorme fende a terra, proba de colleitas pasadas.",
-  "it puts up little resistance before the knife.":
+  "it puts up little resistance before the knife.": 
     "pon pouca resistencia ao coitelo.",
   "there was a beast. it's dead now": "hab\u00eda unha besta. agora est\u00e1 morta",
-  "a shivering man approaches and attacks with surprising strength":
+  "a shivering man approaches and attacks with surprising strength": 
     "un home arrepiante ach\u00e9gase e ataca cunha forza sorprendente",
   "steel's stronger than iron": "o aceiro \u00e9 m\u00e1is forte c\u00f3 ferro",
   "A Strange Bird": "Un Paxaro Estra\u00f1o",
   "not enough alien alloy": "non hai aliaxe alien\u00edxena abonda",
-  "street above the subway platform is blown away.":
+  "street above the subway platform is blown away.": 
     "a r\u00faa que hai por riba da plataforma do metro est\u00e1 destru\u00edda.",
   "the soldier is dead": "o soldado morreu",
-  "error while saving to dropbox datastorage":
+  "error while saving to dropbox datastorage": 
     "houbo un erro ao gardar en dropbox",
   "the footsteps stop.": "os pasos p\u00e1ranse.",
   "sniper": "francotirador",
-  "the torchlight casts a flickering glow down the hallway.":
+  "the torchlight casts a flickering glow down the hallway.": 
     "o facho escintila ao avanzar no corredor.",
   "the warped man lies dead.": "o home deformado xace morto.",
   "something's in the store room": "hai algo no almac\u00e9n",
   "take all you can": "coller todo o que se poida",
-  "unfathomable destruction to fuel wanderer hungers.":
+  "unfathomable destruction to fuel wanderer hungers.": 
     "destruci\u00f3n inconmesurable para saciar a fame dos errantes.",
   "embark": "partir",
   "mourn": "loito",
-  "the trees yield to dry grass. the yellowed brush rustles in the wind.":
-    "as \u00e1rbores deixan paso \u00e1 herba seca. as xestas amarelas marmulan no vento.",
-  "save.": "gardar.",
-  "total score: {0}": "puntos totais: {0}",
-  "learned to make the most of food": "aprendeuse a aproveitar ao m\u00e1ximo a comida",
-  "blast": "abrasi\u00f3n",
-  "the sky is grey and the wind blows relentlessly":
-    "o ceo est\u00e1 gris e o vento sopra desapiadado",
-  "supplies:": "provisi\u00f3ns:",
-  "the feral terror is dead": "o terror salvaxe morreu",
-  "the tracks disappear after just a few minutes.":
-    "o rastro desaparece despois duns minutos.",
-  "a safe place in the wilds.": "un lugar seguro no salvaxe.",
-  "fur": "pel",
-  "beneath the wanderer's rags, clutched in one of its many hands, a glint of steel.":
-    "baixo os farrapos do errante, aferrado a unhas das s\u00faas moitas mans, escintila o aceiro.",
-  "buy scales": "mercar escamas",
-  "mild": "morno",
-  "the hunting lodge stands in the forest, a ways out of town":
-    "o refuxio de caza est\u00e1 no bosque
+  "the trees yield to dry grass. the yellowed brush rustles in the wind.": 
+    "as 
