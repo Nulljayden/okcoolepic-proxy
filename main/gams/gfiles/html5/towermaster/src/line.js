fb249eb59dfb0fe253a1777c7c9e3622b9626516
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:03 -0500

    comment

--- main/gams/gfiles/html5/towermaster/src/line.js
+++ main/gams/gfiles/html5/towermaster/src/line.js
@@ -1,9 +1,19 @@
 import { getMoveDownValue, getLandBlockVelocity } from './utils'
 import * as constant from './constant'
 
+/**
+ * This function handles the actions for the line object in the game.
+ * It sets the initial position of the line when it's not ready,
+ * moves the line downwards based on the moveDownMovement variable,
+ * and updates the position of the line and its collision point based on the landBlockVelocity.
+ * @param {Object} instance - The line object to be updated.
+ * @param {Object} engine - The game engine that contains the game state and utility functions.
+ * @param {number} time - The current time in the game.
+ */
 export const lineAction = (instance, engine, time) => {
   const i = instance
   if (!i.ready) {
+    // Set the initial position of the line
     i.y = engine.getVariable(constant.lineInitialOffset)
     i.ready = true
     i.collisionX = engine.width - engine.getVariable(constant.blockWidth)
@@ -18,23 +28,21 @@ export const lineAction = (instance, engine, time) => {
       name: 'line'
     }
   )
+  // Get the land block velocity and update the position of the line and its collision point
   const landBlockVelocity = getLandBlockVelocity(engine, time)
   instance.x += landBlockVelocity
   instance.collisionX += landBlockVelocity
 }
 
+/**
+ * This function paints the line object on the canvas.
+ * It only executes when the debug mode is enabled.
+ * @param {Object} instance - The line object to be painted.
+ * @param {Object} engine - The game engine that contains the canvas context.
+ */
 export const linePainter = (instance, engine) => {
   const { ctx, debug } = engine
   if (!debug) {
     return
   }
-  ctx.save()
-  ctx.beginPath()
-  ctx.strokeStyle = 'red'
-  ctx.moveTo(instance.x, instance.y)
-  ctx.lineTo(instance.collisionX, instance.y)
-  ctx.lineWidth = 1
-  ctx.stroke()
-  ctx.restore()
-}
-
+  ctx.save
