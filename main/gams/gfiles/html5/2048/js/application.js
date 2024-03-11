b762b9082683fa26f27002339b64d1db99aaa6c4
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:13 -0500

    improve

--- main/gams/gfiles/html5/2048/js/application.js
+++ main/gams/gfiles/html5/2048/js/application.js
@@ -1,4 +1,10 @@
+// Import the required modules
+import GameManager from './GameManager.js';
+import KeyboardInputManager from './KeyboardInputManager.js';
+import HTMLActuator from './HTMLActuator.js';
+import LocalStorageManager from './LocalStorageManager.js';
+
 // Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
+window.requestAnimationFrame(() => {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
