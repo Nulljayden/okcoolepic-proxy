ac8ac586e2f6b2a15c635202e95f85ac0b093ee6
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:20 -0500

    improve

--- main/uv/uv.config.js
+++ main/uv/uv.config.js
@@ -1,11 +1,15 @@
-self.__uv$config = {
-    prefix: '/service/',
-    bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    index: '/uv/uv.index.js',
-    sw: '/uv/uv.sw.js',
+const config = {
+  baseUrl: '/service/',
+  bare: '/bare/',
+  encodeUrl: (data) => Ultraviolet.codec.xor.encode(data),
+  decodeUrl: (data) => Ultraviolet.codec.xor.decode(data),
+  handler: '/uv/uv.handler.js',
+  bundle: '/uv/uv.bundle.js',
+  config: '/uv/uv.config.js',
+  index: '/uv/uv.index.js',
+  sw: '/uv/uv.sw.js',
 };
+
+Object.freeze(config);
+
+module.exports = config;
