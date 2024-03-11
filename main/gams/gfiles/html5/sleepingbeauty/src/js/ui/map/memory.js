342cb90991e754d7b3575990301422870c621695
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:54 -0500

    improve

--- main/gams/gfiles/html5/sleepingbeauty/src/js/ui/map/memory.js
+++ main/gams/gfiles/html5/sleepingbeauty/src/js/ui/map/memory.js
@@ -1,5 +1,6 @@
 import * as cells from "level/cells.js";
 import pc from "being/pc.js";
+import ROT from "rot-js";
 
 const GRASS_1 = new cells.Grass("\"");
 const GRASS_2 = new cells.Grass("'");
@@ -10,50 +11,27 @@ const NOISE = new ROT.Noise.Simplex();
 const memories = {};
 
 function darken(color) {
-	if (!color) { return color; }
-	return ROT.Color.toRGB(ROT.Color.fromString(color).map(x => x>>1));
+  if (!color) {
+    return color;
+  }
+  const rgb = ROT.Color.fromString(color);
+  if (rgb.r.constructor !== Number || rgb.g.constructor !== Number || rgb.b.constructor !== Number) {
+    throw new Error('Invalid color format');
+  }
+  return ROT.Color.toRGB(ROT.Color.fromString(color).map(x => Math.floor(x/2)));
 }
 
 export default class Memory {
-	static forLevel(level) {
-		if (!(level.id in memories)) { memories[level.id] = new this(level); }
-		return memories[level.id];
-	}
+  static forLevel(level) {
+    if (level.constructor !== Object) {
+      throw new Error('Level parameter must be an object');
+    }
+    if (!(level.id in memories)) {
+      memories[level.id] = new this(level);
+    }
+    return memories[level.id];
+  }
 
-	constructor(level) {
-		this._level = level;
-		this._memoized = {};
-	}
-
-	visualAt(xy) {
-		if (this._level.isOutside(xy)) {
-			let entity;
-			let noise = NOISE.get(xy.x/20, xy.y/20);
-			if (noise < 0) {
-				entity = GRASS_1;
-			} else if (noise < 0.8) {
-				entity = GRASS_2;
-			} else {
-				entity = TREE;
-			}
-			return entity.getVisual();
-		}
-
-		let fov = pc.fov;
-		if (xy in fov) {
-			this._memoize(xy, this._level.getCell(xy).getVisual()); // memoize cell only
-			return this._level.getEntity(xy).getVisual();
-		} else if (xy in this._memoized) {
-			return this._memoized[xy];
-		} else {
-			return null;
-		}
-	}
-
-	_memoize(xy, visual) {
-		this._memoized[xy] = {
-			ch: visual.ch,
-			fg: darken(visual.fg)
-		}
-	}
-}
+  constructor(level) {
+    this._level = level;
+    this._memo
