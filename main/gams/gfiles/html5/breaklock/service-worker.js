2b05b9eb25a23054de17236061d549b0df120e78
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:28 -0500

    improve

--- main/gams/gfiles/html5/breaklock/service-worker.js
+++ main/gams/gfiles/html5/breaklock/service-worker.js
@@ -1,7 +1,8 @@
-var APP_NAME = 'breaklock',
-    APP_VERSION = 12,
-    CACHE_NAME = APP_NAME + '_' + APP_VERSION;
-var filesToCache = [
+const APP_NAME = 'breaklock';
+const APP_VERSION = 12;
+const CACHE_NAME = `${APP_NAME}_${APP_VERSION}`;
+
+const filesToCache = [
   './',
   './?utm_source=homescreen',
   './app.css',
@@ -14,23 +15,25 @@ var filesToCache = [
 
 // Service worker from Google Documentation
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', async (event) => {
   // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(filesToCache);
-      })
-  );
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(filesToCache);
+  } catch (error) {
+    console.error('Error during install', error);
+  }
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', async (event) => {
+  const currentClientId = event.clientId;
+
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(async (cacheNames) => {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.indexOf(APP_NAME) === 0 && CACHE_NAME !== cacheName) {
-            return caches.delete(cacheName);
+        cacheNames.map(async (cacheName) => {
+          if (cacheName.startsWith(APP_NAME) && cacheName !== CACHE_NAME) {
+            await caches.delete(cacheName);
           }
         })
       );
@@ -38,42 +41,41 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', async (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
+    caches.match(event.request).then(async (response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      const fetchRequest = event.request.clone();
+
+      try {
+        const response = await fetch(fetchRequest);
+
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        const responseToCache = response.clone();
 
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+        const cache = await caches.open(CACHE_NAME);
+        cache.put(event.request, responseToCache);
 
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+        return response;
+      } catch (error) {
+        console.error('Error during fetch', error);
+      }
+    })
+  );
 });
