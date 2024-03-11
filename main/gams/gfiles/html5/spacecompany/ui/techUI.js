7821d734d80d77bafde75b7c54439080cfac9280
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:58 -0500

    comment

--- main/gams/gfiles/html5/spacecompany/ui/techUI.js
+++ main/gams/gfiles/html5/spacecompany/ui/techUI.js
@@ -1,11 +1,20 @@
+// Create a new techUI module pattern
 Game.techUI = (function() {
+	// Create an instance object to hold all methods and properties
 	var instance = {};
 
+	// Declare a reference to the techTable element
 	instance.techTable = null;
+
+	// Declare a reference to the techTemplate Handlebars template
 	instance.techTemplate = null;
 
+	// Initialize the techUI module
 	instance.initialise = function() {
+		// Assign the techTable variable to the jQuery object of the HTML element with id 'techTable'
 		this.techTable = $('#techTable');
+
+		// Compile the techTemplate Handlebars template using the provided markup
 		this.techTemplate = Handlebars.compile([
 			'<tr id="{{htmlId}}" class="hidden">',
 			'<td>',
@@ -21,28 +30,4 @@ Game.techUI = (function() {
 			'</button>',
 			'<br><br>',
 			'</td>',
-			'</tr>'
-		].join('\n'));
-	};
-
-	instance.addTech = function(data) {
-		var html = this.techTemplate(data);
-		this.techTable.append(html);
-
-		// all currently used techs cost only science
-		var cost = Game.settings.format(data.cost['science']);
-		data.getCostElement().text(cost);
-	};
-
-	instance.removeTech = function(data) {
-		$('#' + data.htmlId).remove();
-	};
-
-	instance.replaceTech = function(data) {
-		// remove the old row first
-		this.removeTech(data);
-		this.addTech(data);
-	};
-
-	return instance;
-}());
+		
