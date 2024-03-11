2288c46037c3f08f7880422ae1333966e6b8bf6c
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:43 -0500

    comment

--- main/gams/gfiles/html5/particleclicker/js/histogram.js
+++ main/gams/gfiles/html5/particleclicker/js/histogram.js
@@ -1,46 +1,66 @@
-
+// A function to draw a histogram of given data.
+// The ident parameter is a selector for the DOM element where the histogram will be drawn.
+// The vals parameter is an array of data values for which the histogram will be generated.
 function draw_hist(ident, vals) {
 
-    // A formatter for counts.
+    // Define a formatter for counts.
     var formatCount = d3.format(",0d");
 
+    // Set the margins and dimensions for the histogram.
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
         width = 400 - margin.left - margin.right,
         height = 200 - margin.top - margin.bottom;
 
+    // Create a linear scale for the x-axis.
+    // The domain is set to [-5, 5] and the range is set to [0, width].
     var x = d3.scale.linear()
         .domain([-5, 5])
         .range([0, width]);
 
     // Generate a histogram using twenty uniformly-spaced bins.
+    // The bins are generated based on the ticks of the x-scale.
     var data = d3.layout.histogram()
         .bins(x.ticks(20))
         (vals);
 
+    // Create a linear scale for the y-axis.
+    // The domain is set based on the maximum value of the data and a correction factor.
+    // The range is set to [height, 0].
     var y = d3.scale.linear()
         .domain([0, d3.max(data, function(d) { return d.y + Math.sqrt(d.y); })])
         .range([height, 0]);
 
+    // Calculate the correction factor for the y-scale.
     correction = y(0);
 
+    // Create an x-axis using the d3.svg.axis function.
+    // The scale is set to the x-scale, the ticks are set to 3, and the orientation is set to "bottom".
     var xAxis = d3.svg.axis()
         .scale(x)
         .ticks(3)
         .tickFormat(function(d) { return '';})
         .orient("bottom");
 
+    // Create a y-axis using the d3.svg.axis function.
+    // The scale is set to the y-scale, the ticks are set to 4, and the orientation is set to "left".
     var yAxis = d3.svg.axis()
         .scale(y)
         .ticks(4)
         //.tickFormat(function(d) { return ''; })
         .orient("left");
 
+    // Append an SVG element to the DOM element selected by ident.
+    // Set the width and height of the SVG element.
+    // Append a g element to the SVG element and translate it by the margins.
     var svg = d3.select(ident).append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // Append circles to the SVG element for each data bin.
+    // The circles are positioned based on the x and y values of the data bin.
+    // The radius of the circles is set to 1.
     var bar = svg.selectAll(".bar")
         .data(data)
         .enter().append("svg:circle")
@@ -49,89 +69,11 @@ function draw_hist(ident, vals) {
         .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
         .attr("r", function(d, i) { return 1 });
 
+    // Append lines to the SVG element for each data bin.
+    // The lines are positioned based on the x and y values of the data bin.
+    // The lines are drawn vertically and horizontally from the center of the circles.
     svg.selectAll(".bar")
         .data(data)
         .enter().append("svg:line")
         .attr("x1", 0)
-        .attr("x2", 0)
-        .attr("y1", function(d) { return y(Math.sqrt(d.y))-correction; })
-        .attr("y2", function(d) { return -y(Math.sqrt(d.y))+correction; })
-        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
-        .attr("stroke", "black")
-        .attr("stroke-width", 1)
-
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("svg:line")
-        .attr("x1", -2)
-        .attr("x2", 2)
-        .attr("y1", function(d) { return y(Math.sqrt(d.y))-correction; })
-        .attr("y2", function(d) { return y(Math.sqrt(d.y))-correction; })
-        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
-        .attr("stroke", "black")
-        .attr("stroke-width", 1)
-
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("svg:line")
-        .attr("x1", -2)
-        .attr("x2", 2)
-        .attr("y1", function(d) { return -y(Math.sqrt(d.y))+correction; })
-        .attr("y2", function(d) { return -y(Math.sqrt(d.y))+correction; })
-        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
-        .attr("stroke", "black")
-        .attr("stroke-width", 1)
-
-    bar.append("rect")
-        .attr("x", 1)
-        .attr("width", x(data[0].dx))
-        .attr("height", function(d) { return height - y(d.y); });
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height ) + ")")
-        .call(xAxis);
-
-    svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height + 20)
-        .text("m (GeV)");
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(0, 0)")
-        .call(yAxis);
-
-    svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("y", -20)
-        .attr("dy", "-1em")
-        .attr("dx", "-0.5em")
-        .attr("transform", "rotate(-90)")
-        .text("events");
-
-    return svg;
-}
-
-function Histogram(ident) {
-    // TODO: Allow for arbitrary numbers of events
-    this.values = [];
-    draw_hist(ident, []);
-
-    this.add_events = function(num) {
-        var new_vals = d3.range(num).map(d3.random.normal(0, 1));
-        this.values = $.merge(this.values, new_vals);
-        d3.select(ident).select("svg").remove();
-        draw_hist(ident, this.values);
-    }
-
-    this.clear = function() {
-        d3.select(ident).select("svg").remove();
-        this.values = [];
-        draw_hist(ident, []);
-    }
-}
-
+        .attr("x2
