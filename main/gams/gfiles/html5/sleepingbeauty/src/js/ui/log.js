2d10a044988f14b431ce1ad271d1dc2b7e5c16ac
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:50 -0500

    improve

--- main/gams/gfiles/html5/sleepingbeauty/src/js/ui/log.js
+++ main/gams/gfiles/html5/sleepingbeauty/src/js/ui/log.js
@@ -1,33 +1,51 @@
 let node;
 let current = null;
 
-export function add() {
-	let str = String.format.apply(String, arguments);
-	str = str.replace(/{(.*?)}(.*?){}/g, (match, color, str) => {
-		return `<span style="color:${color}">${str}</span>`;
-	});
-	str = str.replace(/\n/g, "<br/>");
-	
-	let item = document.createElement("span");
-	item.innerHTML = `${str} `;
-	current.appendChild(item);
+export function add(str) {
+  if (typeof str !== 'string') {
+    throw new Error('The input argument must be a string');
+  }
+
+  str = str.replace(/{(.*?)}(.*?){}/g, (match, color, str) => {
+    return `<span style="color:${color}">${str}</span>`;
+  });
+
+  str = str.replace(/\n/g, "<br/>");
+
+  let item = document.createElement("span");
+  item.innerHTML = `${str} `;
+  if (current) {
+    current.appendChild(item);
+  } else {
+    throw new Error('No node has been initialized');
+  }
 }
 
 export function pause() {
-	if (current && current.childNodes.length == 0) { return; }
-	current = document.createElement("p");
-	node.appendChild(current);
-	
-	while (node.childNodes.length > 50) { node.removeChild(node.firstChild); }
+  if (!current || current.childNodes.length > 0) { return; }
+  current = document.createElement("p");
+  if (node) {
+    node.appendChild(current);
+  } else {
+    throw new Error('No node has been initialized');
+  }
+
+  while (node.childNodes.length > 50) {
+    node.removeChild(node.firstChild);
+  }
 }
 
 export function init(n) {
-	node = n;
-	node.classList.remove("hidden");
+  if (n.constructor.name !== 'HTMLDivElement') {
+    throw new Error('The input argument must be an HTMLDivElement');
+  }
 
-	pause();
+  node = n;
+  node.classList.remove("hidden");
 
-	setInterval(() => {
-		node.scrollTop += 3;
-	}, 20);
+  pause();
+
+  setInterval(() => {
+    node.scrollTop += 3;
+  }, 20);
 }
