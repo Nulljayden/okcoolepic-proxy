377df19aa444466091c456da14afa8a2a93b5fb6
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:58 -0500

    comment

--- main/gams/gfiles/html5/tetris/Button.js
+++ main/gams/gfiles/html5/tetris/Button.js
@@ -1,13 +1,22 @@
+// Define a new function called Button which takes a config object as its parameter
 function Button(config) {
 
+    // Create a new instance of jaws.Sprite using the provided config object
     var parent = new jaws.Sprite(config),
-    key;
 
+        // Declare a variable called key
+        key;
+
+    // Iterate over each property in the parent object
     for (key in parent) {
-	this[key] = parent[key];
+        // Copy each property from the parent object to the current object
+        this[key] = parent[key];
     }
 }
 
+// Add a new method to the Button prototype called isClicked
 Button.prototype.isClicked = function(x, y) {
+    // Return true if the point (x, y) is inside the button's rectangle, false otherwise
     return this.rect().collidePoint(x, y);
 };
+
