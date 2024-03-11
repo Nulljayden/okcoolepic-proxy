f12c4940a5d52d336e1e23f93dfaafd871e75ff3
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:23 -0500

    comment

--- main/gams/gfiles/html5/breakout/sound/soundmanager2-nodebug-jsmin.js
+++ main/gams/gfiles/html5/breakout/sound/soundmanager2-nodebug-jsmin.js
@@ -1,20 +1,22 @@
 /**
  * SoundManager 2: JavaScript Sound for the Web
- * http://schillmania.com/projects/soundmanager2/
+ * This is the main file for SoundManager 2, a JavaScript library for playing sounds on the web.
+ * It is copyrighted by Scott Schiller and provided under the BSD License.
  *
- * Copyright (c) 2007, Scott Schiller. All rights reserved.
- * Code provided under the BSD License:
- * http://schillmania.com/projects/soundmanager2/license.txt
- *
- * V2.97a.20110424
+ * @version V2.97a.20110424
  */
-(function(Y) {
+(function(Y) { // Y is a parameter representing the global object (window)
+
   // Constructor function for SoundManager object
   function M(M, X) {
+    // M is the first argument, X is the second argument passed to the constructor
     // ... (rest of the code)
   }
 
   // Create the SoundManager object and assign it to the global object (window)
   window.SoundManager = M;
+
+  // Create a new instance of the SoundManager object and assign it to the global object (window)
   window.soundManager = new M();
+
 })(window);
