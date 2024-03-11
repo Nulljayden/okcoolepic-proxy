ca6f524475fa08d9c002c65168a0fddf1701f707
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:18 -0500

    improve

--- main/js/load.js
+++ main/js/load.js
@@ -1,1 +1,12 @@
-/* literally useless */
+// This is a useful function that prints a greeting message to the console.
+
+#include <stdio.h>
+
+void print_greeting() {
+  printf("Hello, world! Welcome to my program.\n");
+}
+
+int main() {
+  print_greeting();
+  return 0;
+}
