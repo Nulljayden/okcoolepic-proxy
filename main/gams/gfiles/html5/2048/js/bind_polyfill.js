403733aec8113271ba2fbd48717fe965e3308ac6
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:12 -0500

    improve

--- main/gams/gfiles/html5/2048/js/bind_polyfill.js
+++ main/gams/gfiles/html5/2048/js/bind_polyfill.js
@@ -1,9 +1,18 @@
-Function.prototype.bind = Function.prototype.bind || function (target) {
-  var self = this;
-  return function (args) {
-    if (!(args instanceof Array)) {
-      args = [args];
+Function.prototype.myBind = Function.prototype.myBind || function (target, ...args) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Bind must be called on a function');
+  }
+
+  const self = this;
+  const boundArgs = args.slice(1);
+
+  const boundFunction = function (...callArgs) {
+    if (this instanceof boundFunction) {
+      return new self(...boundArgs, ...callArgs);
     }
-    self.apply(target, args);
+
+    return self.apply(target, boundArgs.concat(callArgs));
   };
+
+  return boundFunction;
 };
