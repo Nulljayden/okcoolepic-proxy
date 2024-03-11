94a569a84f8a2375bb4ef1ede1603d0b6f229782
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:32 -0500

    improve

--- main/gams/gfiles/html5/adarkroom/lib/icu.js
+++ main/gams/gfiles/html5/adarkroom/lib/icu.js
@@ -1,23 +1,60 @@
 (function() {
+  // Define the date and number formats
+  const dateFormats = {
+    SHORT_PADDED_CENTURY: d => `${d.getDate().toString().padStart(2, '0')}/${(d.getMonth() + 1).toString().padStart(2, '0')}/${d.getFullYear()}`,
+    SHORT: d => `${d.getDate().toString().padStart(2, '0')}/${(d.getMonth() + 1).toString().padStart(2, '0')}/${d.getFullYear().toString().slice(-2)}`,
+    SHORT_NOYEAR: d => `${d.getDate().toString().padStart(2, '0')}/${(d.getMonth() + 1).toString().padStart(2, '0')}`,
+    SHORT_NODAY: d => `${(d.getMonth() + 1).toString().padStart(2, '0')}/${d.getFullYear().toString().slice(-2)}`,
+    MEDIUM: d => `${d.getDate()} ${dateFormatSymbols.month_short[d.getMonth()]} ${d.getFullYear()}`,
+    MEDIUM_NOYEAR: d => `${d.getDate()} ${dateFormatSymbols.month_short[d.getMonth()]}`,
+    MEDIUM_WEEKDAY_NOYEAR: d => `${dateFormatSymbols.day_short[d.getDay()]} ${d.getDate()} ${dateFormatSymbols.month_short[d.getMonth()]}`,
+    LONG_NODAY: d => `${dateFormatSymbols.month_name[d.getMonth()]} ${d.getFullYear()}`,
+    LONG: d => `${d.getDate()} ${dateFormatSymbols.month_name[d.getMonth()]} ${d.getFullYear()}`,
+    FULL: d => `${dateFormatSymbols.day_name[d.getDay()]} ${d.getDate()} ${dateFormatSymbols.month_name[d.getMonth()]} ${d.getFullYear()}`,
+  };
+  const numberFormatSymbols = {
+    decimal_separator: ',',
+    grouping_separator: ' ',
+    minus: '-',
+  };
+  const dateFormatSymbols = {
+    am_pm: ['AM', 'PM'],
+    day_name: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
+    day_short: ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.'],
+    era: ['av. J.-C.', 'ap. J.-C.'],
+    era_name: ['avant Jésus-Christ', 'après Jésus-Christ'],
+    month_name: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+    month_short: ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'],
+    order_full: 'DMY',
+    order_long: 'DMY',
+    order_medium: 'DMY',
+    order_short: 'DMY',
+  };
 
-	var dfs = {"am_pm":["AM","PM"],"day_name":["dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi"],"day_short":["dim.","lun.","mar.","mer.","jeu.","ven.","sam."],"era":["av. J.-C.","ap. J.-C."],"era_name":["avant Jésus-Christ","après Jésus-Christ"],"month_name":["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"],"month_short":["janv.","févr.","mars","avr.","mai","juin","juil.","août","sept.","oct.","nov.","déc."],"order_full":"DMY","order_long":"DMY","order_medium":"DMY","order_short":"DMY"};
-	var nfs = {"decimal_separator":",","grouping_separator":" ","minus":"-"};
-	var df = {SHORT_PADDED_CENTURY:function(d){if(d){return(((d.getDate()+101)+'').substring(1)+'/'+((d.getMonth()+101)+'').substring(1)+'/'+d.getFullYear());}},SHORT:function(d){if(d){return(((d.getDate()+101)+'').substring(1)+'/'+((d.getMonth()+101)+'').substring(1)+'/'+(d.getFullYear()+'').substring(2));}},SHORT_NOYEAR:function(d){if(d){return(((d.getDate()+101)+'').substring(1)+'/'+((d.getMonth()+101)+'').substring(1));}},SHORT_NODAY:function(d){if(d){return(((d.getMonth()+101)+'').substring(1)+'/'+(d.getFullYear()+'').substring(2));}},MEDIUM:function(d){if(d){return(d.getDate()+' '+dfs.month_short[d.getMonth()]+' '+d.getFullYear());}},MEDIUM_NOYEAR:function(d){if(d){return(d.getDate()+' '+dfs.month_short[d.getMonth()]);}},MEDIUM_WEEKDAY_NOYEAR:function(d){if(d){return(dfs.day_short[d.getDay()]+' '+d.getDate()+' '+dfs.month_short[d.getMonth()]);}},LONG_NODAY:function(d){if(d){return(dfs.month_name[d.getMonth()]+' '+d.getFullYear());}},LONG:function(d){if(d){return(d.getDate()+' '+dfs.month_name[d.getMonth()]+' '+d.getFullYear());}},FULL:function(d){if(d){return(dfs.day_name[d.getDay()]+' '+d.getDate()+' '+dfs.month_name[d.getMonth()]+' '+d.getFullYear());}}};
-	
-	window.icu = window.icu || new Object();
-	var icu = window.icu;	
-		
-	icu.getCountry = function() { return ""; };
-	icu.getCountryName = function() { return ""; };
-	icu.getDateFormat = function(formatCode) { var retVal = {}; retVal.format = df[formatCode]; return retVal; };
-	icu.getDateFormats = function() { return df; };
-	icu.getDateFormatSymbols = function() { return dfs; };
-	icu.getDecimalFormat = function(places) { var retVal = {}; retVal.format = function(n) { var ns = n < 0 ? Math.abs(n).toFixed(places) : n.toFixed(places); var ns2 = ns.split('.'); s = ns2[0]; var d = ns2[1]; var rgx = /(\d+)(\d{3})/;while(rgx.test(s)){s = s.replace(rgx, '$1' + nfs["grouping_separator"] + '$2');} return (n < 0 ? nfs["minus"] : "") + s + nfs["decimal_separator"] + d;}; return retVal; };
-	icu.getDecimalFormatSymbols = function() { return nfs; };
-	icu.getIntegerFormat = function() { var retVal = {}; retVal.format = function(i) { var s = i < 0 ? Math.abs(i).toString() : i.toString(); var rgx = /(\d+)(\d{3})/;while(rgx.test(s)){s = s.replace(rgx, '$1' + nfs["grouping_separator"] + '$2');} return i < 0 ? nfs["minus"] + s : s;}; return retVal; };
-	icu.getLanguage = function() { return "fr"; };
-	icu.getLanguageName = function() { return "français"; };
-	icu.getLocale = function() { return "fr"; };
-	icu.getLocaleName = function() { return "français"; };
+  // Freeze the format objects to prevent modification
+  Object.freeze(dateFormats);
+  Object.freeze(numberFormatSymbols);
+  Object.freeze(dateFormatSymbols);
 
-})();
+  // Define the ICU object
+  const icu = {
+    getCountry() { return ''; },
+    getCountryName() { return ''; },
+    getDateFormat(formatCode) {
+      if (!formatCode || !dateFormats[formatCode]) {
+        throw new Error(`Invalid date format code: ${formatCode}`);
+      }
+      return { format: dateFormats[formatCode] };
+    },
+    getDateFormats() { return dateFormats; },
+    getDateFormatSymbols() { return dateFormatSymbols; },
+    getDecimalFormat(places) {
+      if (!places || typeof places !== 'number') {
+        throw new Error(`Invalid number of decimal places: ${places}`);
+      }
+      return {
+        format: n => {
+          const abs = Math.abs(n);
+          const int = abs.toString().split('.')[0];
+          const dec = abs.toString().split('.')[1] || '';
+          const intParts = int.split('').reverse().map((digit, i) => i % 3 === 0 && i !== 0 ? `${digit}${numberFormatSymbols.grouping
