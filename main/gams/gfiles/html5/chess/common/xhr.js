f68a9f57f9f9336f0993ce9fac2028d4e546bd45
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:26 -0500

    improve

--- main/gams/gfiles/html5/chess/common/xhr.js
+++ main/gams/gfiles/html5/chess/common/xhr.js
@@ -1,32 +1,43 @@
-
-// retrieves a file via XMLHTTPRequest, calls fncCallback when done or fncError on error.
-
-function XHR(strURL, fncCallback /*, argumentToPass1, argumentToPass2, etc. */) {
-	var oHTTP, argsArr = Array.prototype.slice.call(arguments, 2);
-	if (window.XMLHttpRequest) { oHTTP = new XMLHttpRequest(); }
-	else if (window.ActiveXObject) { oHTTP = new ActiveXObject("Microsoft.XMLHTTP"); }
-	if (oHTTP) {
-		if (fncCallback) {
-			if (typeof(oHTTP.onload) !== "undefined")
-				oHTTP.onload = function() {
-					fncCallback.apply(oHTTP, argsArr);
-					oHTTP = null;
-				};
-			else {
-				oHTTP.onreadystatechange = function() {
-					if (oHTTP.readyState === 4) {
-						fncCallback.apply(oHTTP, argsArr);
-						oHTTP = null;
-					}
-				};
-			}
-		}
-		oHTTP.open("GET", strURL, true);
-  		oHTTP.setRequestHeader("Content-Type", "text/plain");
-		oHTTP.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
-		oHTTP.send(null);
-	}
+// retrieves a file via XMLHttpRequest, calls fncCallback when done or fncError on error.
+function XHR(strURL, fncCallback, ...args) {
+  const oHTTP = new XMLHttpRequest();
+  if (oHTTP) {
+    oHTTP.open("GET", strURL, true);
+    oHTTP.responseType = "text";
+    oHTTP.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        fncCallback(this.response, ...args);
+      } else {
+        fncError(this.statusText);
+      }
+    };
+    oHTTP.onerror = function() {
+      fncError("Network error");
+    };
+    oHTTP.ontimeout = function() {
+      fncError("Timeout error");
+    };
+    oHTTP.setRequestHeader("Content-Type", "text/plain");
+    oHTTP.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
+    oHTTP.send();
+  }
 }
 
-function setAttribs() { for (var iAttr = 0; iAttr < arguments.length; iAttr++) { this[arguments[iAttr][0]] = arguments[iAttr][1]; } return(this); }
-function setStyles() { for (var iPropr = 0; iPropr < arguments.length; iPropr++) { this.style[arguments[iPropr][0]] = arguments[iPropr][1]; } return(this); }
+function setAttribs(...args) {
+  if (args.length % 2 !== 0) {
+    throw new Error("Invalid number of arguments");
+  }
+  for (let i = 0; i < args.length; i += 2) {
+    this.setAttribute(args[i], args[i + 1]);
+  }
+  return this;
+}
+
+function setStyles(...args) {
+  if (args.length % 2 !== 0) {
+    throw new Error("Invalid number of arguments");
+  }
+  for (let i = 0; i < args.length; i += 2) {
+    this.style[args[i]] = args[i + 1];
+  }
+  return this;
