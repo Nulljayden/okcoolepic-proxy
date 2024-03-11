41e9373613b13f05e888658ee03cba300d9f4aae
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:00 -0500

    improve

--- main/gams/gfiles/html5/spacecompany/building.js
+++ main/gams/gfiles/html5/spacecompany/building.js
@@ -1,17 +1,41 @@
-Game.buildings = (function(){
+/**
+ * Game.buildings module
+ *
+ * Handles the creation, updating, and saving of game buildings.
+ */
+Game.buildings = (function() {
 
-    var instance = {};
+    /**
+     * The buildings module instance.
+     * @type {Object}
+     */
+    const instance = {};
 
+    /**
+     * The version number of the data format.
+     * @type {number}
+     */
     instance.dataVersion = 1;
-    instance.entries = {};
-    instance.updatePerSecondProduction = true;
-    instance.techTypeCount = 0;
 
+    /**
+     * The building data entries.
+     * @type {Object}
+     */
+    instance.entries = {};
+
+    /**
+     * Whether to update production per second.
+     * @type {boolean}
+     */
+    instance.updatePerSecondProduction = true;
+
+    /**
+     * Initializes the buildings module.
+     */
     instance.initialise = function() {
-        for (var id in Game.buildingData) {
-            var data = Game.buildingData[id];
-            this.techTypeCount++;
-            this.entries[id] = $.extend({}, data, {
+        for (const id in Game.buildingData) {
+            const data = Game.buildingData[id];
+            instance.entries[id] = Object.assign({}, data, {
                 id: id,
                 htmlId: 'resbld_' + id,
                 current: 0,
@@ -19,82 +43,96 @@ Game.buildings = (function(){
                 iconName: data.icon,
                 iconExtension: Game.constants.iconExtension,
                 max: data.maxCount,
-                displayNeedsUpdate: true
             });
         }
 
-        console.debug("Loaded " + this.techTypeCount + " Building Types");
+        console.debug("Loaded " + Object.keys(instance.entries).length + " Building Types");
     };
 
+    /**
+     * Updates the buildings module.
+     * @param {number} delta - The time delta in seconds.
+     */
     instance.update = function(delta) {
-        if (this.updatePerSecondProduction === true) {
-            this.updateProduction();
+        if (instance.updatePerSecondProduction === true) {
+            instance.updateProduction();
         }
     };
 
+    /**
+     * Saves the buildings module data.
+     * @param {Object} data - The data object to save to.
+     */
     instance.save = function(data) {
-        data.buildings = { v: this.dataVersion, i: {}};
-        for(var key in this.entries) {
-            data.buildings.i[key] = this.entries[key].current;
+        data.buildings = { v: instance.dataVersion, i: {} };
+        for (const key in instance.entries) {
+            data.buildings.i[key] = instance.entries[key].current;
         }
     };
 
+    /**
+     * Loads the buildings module data.
+     * @param {Object} data - The data object to load from.
+     */
     instance.load = function(data) {
-        if(data.buildings) {
-            if(data.buildings.v && data.buildings.v === this.dataVersion) {
-                for(var id in data.buildings.i) {
-                    if(this.entries[id]) {
-                        this.constructBuildings(id, data.buildings.i[id]);
+        if (data.buildings) {
+            if (data.buildings.v && data.buildings.v === instance.dataVersion) {
+                for (const id in data.buildings.i) {
+                    if (instance.entries[id]) {
+                        instance.constructBuildings(id, data.buildings.i[id]);
                     }
                 }
             }
         }
     };
 
+    /**
+     * Constructs buildings.
+     * @param {string} id - The building ID.
+     * @param {number} count - The number of buildings to construct.
+     */
     instance.constructBuildings = function(id, count) {
         // Add the buildings and clamp to the maximum
-        var newValue = Math.floor(this.entries[id].current + count);
-        this.entries[id].current = Math.min(newValue, this.entries[id].max);
-        this.entries[id].displayNeedsUpdate = true;
-        this.updatePerSecondProduction = true;
+        const newValue = Math.min(instance.entries[id].current + count, instance.entries[id].max);
+        instance.entries[id].current = newValue;
+        instance.updatePerSecondProduction = true;
     };
 
+    /**
+     * Destroys buildings.
+     * @param {string} id - The building ID.
+     * @param {number} count - The number of buildings to destroy.
+     */
     instance.destroyBuildings = function(id, count) {
         // Remove the buildings and ensure we can not go below 0
-        var newValue = Math.floor(this.entries[id].current - count);
-        this.entries[id].current = Math.max(newValue, 0);
-        this.entries[id].displayNeedsUpdate = true;
-        this.updatePerSecondProduction = true;
+        const newValue = Math.max(instance.entries[id].current - count, 0);
+        instance.entries[id].current = newValue;
+        instance.updatePerSecondProduction = true;
     };
 
+    /**
+     * Unlocks a building.
+     * @param {string} id - The building ID.
+     */
     instance.unlock = function(id) {
-        this.entries[id].unlocked = true;
-        this.entries[id].displayNeedsUpdate = true;
+        instance.entries[id].unlocked = true;
     };
 
+    /**
+     * Updates the production per second.
+     */
     instance.updateProduction = function() {
-        for(var id in this.entries) {
-            var data = this.entries[id];
-            if(data.current == 0) {
-                // Nothing to be done
+        for (const id in instance.entries) {
+            const data = instance.entries[id];
+            if (data.current == 0 || !data.resource) {
                 continue;
             }
 
-            var buildingData = this.entries[id];
-            if (!buildingData.resource) {
-                continue;
-            }
-
-            var baseValue = data.current * buildingData.perSecond;
-            Game.resources.setPerSecondProduction(buildingData.resource, baseValue);
+            const baseValue = data.current * (data.perSecond || 0);
+            Game.resources.setPerSecondProduction(data.resource, baseValue);
         }
 
-        this.updatePerSecondProduction = false;
+        instance.updatePerSecondProduction = false;
     };
 
-    instance.getBuildingData = function(id) {
-        return this.entries[id];
-    };
-
-    return instance;
-}());
+    /**
