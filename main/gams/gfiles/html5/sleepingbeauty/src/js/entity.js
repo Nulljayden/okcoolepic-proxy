510ed367d1f68c08448504c59bd4e31dc18604b5
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:48 -0500

    comment

--- main/gams/gfiles/html5/sleepingbeauty/src/js/entity.js
+++ main/gams/gfiles/html5/sleepingbeauty/src/js/entity.js
@@ -1,18 +1,7 @@
+// Define three different types of blocks using bitwise constants
 export const BLOCKS_NONE = 0;
-export const BLOCKS_MOVEMENT = 1;
-export const BLOCKS_LIGHT = 2;
+export const BLOCKS_MOVEMENT = 1; // Represents blocks that restrict movement
+export const BLOCKS_LIGHT = 2; // Represents blocks that restrict light
 
-export default class Entity {
-	constructor(visual) {
-		this._visual = visual;
-		this.blocks = BLOCKS_NONE; 
-	}
-
-	getVisual() { return this._visual; }
-
-	toString() { return this._visual.name; }
-
-	describe(articleType) {
-		if (!articleType || !this[`describe${articleType}`]) {
-			throw new Error(`Invalid article type: ${articleType}`);
-	
+// Default class representing an entity with a visual component
+//
