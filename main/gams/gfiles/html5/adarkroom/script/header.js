c3b46deda0fe6d3a54e77e1665fe4fbb0894653b
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:20 -0500

    improve

--- main/gams/gfiles/html5/adarkroom/script/header.js
+++ main/gams/gfiles/html5/adarkroom/script/header.js
@@ -1,28 +1,38 @@
 /**
- * Module that takes care of header buttons
+ * Module that handles header buttons
  */
-var Header = {
-	
-	init: function(options) {
-		this.options = $.extend(
-			this.options,
-			options
-		);
-	},
-	
-	options: {}, // Nothing for now
-	
-	canTravel: function() {
-		return $('div#header div.headerButton').length > 1;
-	},
-	
-	addLocation: function(text, id, module) {
-		return $('<div>').attr('id', "location_" + id)
-			.addClass('headerButton')
-			.text(text).click(function() {
-				if(Header.canTravel()) {
-					Engine.travelTo(module);
-				}
-			}).appendTo($('div#header'));
-	}
-};
+const Header = (() => {
+  let idCounter = 0; // Counter for unique IDs
+
+  const init = (options) => {
+    options = { ...options }; // Make a copy of options to avoid modifying the original object
+    // ... any other initialization code ...
+  };
+
+  const options = {}; // Nothing for now
+
+  const canTravel = () => {
+    return $('div#header div.headerButton').length > 1;
+  };
+
+  const addLocation = (text, module) => {
+    const id = `location_${idCounter++}`; // Generate a unique ID
+    const button = $('<div>').attr('id', id)
+      .addClass('headerButton')
+      .text(text)
+      .click(() => {
+        if (canTravel()) {
+          Engine.travelTo(module);
+        }
+      });
+    $('div#header').append(button);
+    return button;
+  };
+
+  init(); // Initialize the module
+
+  return {
+    addLocation,
+    canTravel,
+  };
+})();
