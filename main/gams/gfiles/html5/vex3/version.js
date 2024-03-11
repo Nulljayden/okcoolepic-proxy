262ec33baaba55078cdefd5a6a2b20422a397542
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:09 -0500

    comment

--- main/gams/gfiles/html5/vex3/version.js
+++ main/gams/gfiles/html5/vex3/version.js
@@ -1,2 +1,5 @@
+// Set the version number to 16
 version="16";
-libs=["./cdn.jsdelivr.net/npm/@orange-games/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js","./cdn.jsdelivr.net/npm/@orange-games/phaser-spine@3.0/build/phaser-spine.min.js","./cdn.jsdelivr.net/npm/@orange-games/phaser-super-storage@1.0/build/phaser-super-storage.min.js","./cdn.jsdelivr.net/npm/@orange-games/splash@3.5/build/splash.min.js"];
+
+// Define an array of library URLs to be loaded
+libs=["
