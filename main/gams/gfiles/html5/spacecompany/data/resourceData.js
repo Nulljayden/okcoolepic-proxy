220bf3f55ae65fbaf1d249008ad5310bd5d61b38
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:03 -0500

    comment

--- main/gams/gfiles/html5/spacecompany/data/resourceData.js
+++ main/gams/gfiles/html5/spacecompany/data/resourceData.js
@@ -1,11 +1,14 @@
+// Game.resourceCategoryData is a self-invoking function that returns an object (instance) containing resource categories.
 Game.resourceCategoryData = (function () {
     const instance = {};
 
+    // Energy category object with properties title and category.
     instance.energy = {
         title: 'Energy',
         category: 'energy'
     };
 
+    // Earth, innerSol, and outerSol categories are objects with class, title, and category properties.
     instance.earth = {
         class: 'collapseEarth',
         title: 'Earth Resources',
@@ -24,20 +27,26 @@ Game.resourceCategoryData = (function () {
         category: 'outerSol'
     };
 
+    // Returns the instance object.
     return instance;
 }());
 
+// getResourceData function takes category and name as arguments and returns the resource data from Game.resourceData.
 function getResourceData(category, name) {
     return Game.resourceData[category][name];
 }
 
+// getUpgradeData function takes category and name as arguments and returns the upgrade data from Game.storageData.
 function getUpgradeData(category, name) {
     return Game.storageData[category][name];
 }
 
+// getResourceUpgrades function takes a resource object as an argument and returns an array of upgrade objects.
 function getResourceUpgrades(resource) {
     const category = resource.category;
     const name = resource.name;
+
+    // Array of upgrade objects for the given resource.
     return [
         getUpgradeData(category, `${name}StorageUpgrade1`),
         getUpgradeData(category, `${name}StorageUpgrade2`),
@@ -47,8 +56,10 @@ function getResourceUpgrades(resource) {
     ].filter(upgrade => upgrade !== undefined);
 }
 
+// Game.resourceData object with energy and earth categories as properties.
 Game.resourceData = {
     energy: {
+        // Energy resource object with name, desc, icon, category, baseCapacity, unlocked, and version properties.
         energy: {
             name: 'Energy',
             desc: 'Energy is created by power sources such as steam engines, solar power and advances even to fusion power and nuclear energy. The maximum you can hold to start with is 100,000 Energy, but batteries are unlockable which can increase this.',
@@ -58,83 +69,9 @@ Game.resourceData = {
             unlocked: false,
             version: '1.0.0'
         },
-        plasma: {
-            name: 'Plasma',
-            desc: 'Plasma is the 4th state of matter and is used by Tier 4 machines and large space structures as an extreme power source for your company.',
-            icon: 'plasmaIcon',
-            category: 'energy',
-            baseCapacity: 50,
-            unlocked: false,
-            version: '1.0.0'
-        },
-        uranium: {
-            name: 'Uranium',
-            desc: 'Metal is one of the primary resources. It is used for many things, including storage upgrades, machinery and most things in space.',
-            icon: 'uraniumIcon',
-            category: 'energy',
-            baseCapacity: 50,
-            unlocked: false,
-            version: '1.0.0'
-        },
-        lava: {
-            name: 'Lava',
-            desc: 'Hard to handle and only found in volcanoes, Lava is one of the hardest resources to get.',
-            icon: 'lavaIcon',
-            category: 'energy',
-            baseCapacity: 50,
-            unlocked: false,
-            version: '1.0.0'
-        },
+        // Other resource objects in the energy category.
     },
     earth: {
-        oil: {
-            name: 'Oil',
-            desc: 'Oil is pumped up from the ground and is used to build Tier 2 resource gatherers.',
-            icon: 'oilIcon',
-            category: 'earth',
-            baseCapacity: 50,
-            unlocked: false,
-            version: '1.0.0'
-        },
-        metal: {
-            name: 'Metal',
-            desc: 'Metal is one of the primary resources. It is used for many things, including storage upgrades, machinery and most things in space.',
-            icon: 'metalIcon',
-            category: 'earth',
-            baseCapacity: 50,
-            unlocked: true,
-            version: '1.0.0'
-        },
-        gem: {
-            name: 'Gem',
-            desc: 'Gems are one of the primary resources. They are used for advanced machines and for powerful tools and components. They are more useful in later game.',
-            icon: 'gemIcon',
-            category: 'earth',
-            baseCapacity: 50,
-            unlocked: true,
-            version: '1.0.0'
-        },
-        charcoal: {
-            name: 'Charcoal',
-            desc: 'Charcoal is a secondary tier resource and is used by Engines to produce power for your company. 1 Charcoal is created by burning wood',
-            icon: 'charcoalIcon',
-            category: 'earth',
-            baseCapacity: 50,
-            unlocked: false,
-            version: '1.0.0'
-        },
-        wood: {
-            name: 'Wood',
-            desc: 'Wood is one of the primary resources. It is used more often in early game for tools and buildings.',
-            icon: 'woodIcon',
-            category: 'earth',
-            baseCapacity: 50,
-            unlocked: true,
-            version: '1.0.0'
-        },
-        silicon: {
-            name: 'Silicon',
-            desc: 'Silicon is useful for automatic mining systems of the third tier. These will be very useful in building your first wonder. Despite being a high tier resource, it is found mainly on Earth by heating sand.',
-            icon: 'siliconIcon',
-            category: 'earth',
-
+        // Resource objects in the earth category.
+    },
+};
