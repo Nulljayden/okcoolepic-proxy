d60cb7e507a0a1061091c4989923bebe3b5b8202
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:12 -0500

    comment

--- main/gams/gfiles/html5/2048/js/tile.js
+++ main/gams/gfiles/html5/2048/js/tile.js
@@ -1,27 +1,38 @@
+// Constructor function for creating a new Tile object with a given position and value
 function Tile(position, value) {
-  this.x                = position.x;
-  this.y                = position.y;
-  this.value            = value || 2;
+  // The x coordinate of the tile's position
+  this.x = position.x;
+  // The y coordinate of the tile's position
+  this.y = position.y;
+  // The value of the tile, initialized to the given value or 2 if not provided
+  this.value = value || 2;
 
+  // The previous position of the tile, used for tracking movement
   this.previousPosition = null;
-  this.mergedFrom       = null; // Tracks tiles that merged together
+  // The tile that this tile merged with, used for tracking merged tiles
+  this.mergedFrom = null;
 }
 
+// A prototype method for saving the current position of the tile
 Tile.prototype.savePosition = function () {
   this.previousPosition = { x: this.x, y: this.y };
 };
 
+// A prototype method for updating the position of the tile
 Tile.prototype.updatePosition = function (position) {
   this.x = position.x;
   this.y = position.y;
 };
 
+// A prototype method for serializing the tile into a plain object
 Tile.prototype.serialize = function () {
   return {
+    // The serialized position of the tile
     position: {
       x: this.x,
       y: this.y
     },
+    // The serialized value of the tile
     value: this.value
   };
 };
