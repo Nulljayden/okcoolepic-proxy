dfde46096b401c2b6dec9b6fcb448b39c7529499
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:57 -0500

    improve

--- main/gams/gfiles/html5/towermaster/index.js
+++ main/gams/gfiles/html5/towermaster/index.js
@@ -1,17 +1,22 @@
-const express = require('express')
-const path = require('path')
-const opn = require('opn')
+const express = require('express');
+const path = require('path');
+const opn = require('opn');
 
-const server = express()
-const host = 'http://localhost:8082'
-server.use('/assets', express.static(path.resolve(__dirname, './assets')))
-server.use('/dist', express.static(path.resolve(__dirname, './dist')))
+const app = express();
+const port = 8082;
+const host = `http://localhost:${port}`;
 
-server.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'));
-})
+app.use('/assets', express.static(path.join(process.cwd(), 'assets')));
+app.use('/dist', express.static(path.join(process.cwd(), 'dist')));
 
-server.listen(8082, () => {
-  console.log(`server started at ${host}`)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server started at ${host}`);
   opn(host)
-})
+    .catch((err) => {
+      console.error(`Failed to open ${host} in the default browser: ${err}`);
+    });
+});
