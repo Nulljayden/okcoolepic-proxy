7005c7bd27e239b9389763a4ca6b1a7393dd9c9f
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:24 -0500

    improve

--- main/sw.js
+++ main/sw.js
@@ -1,9 +1,23 @@
 importScripts('./uv/uv.sw.js');
 
-const sw = new UVServiceWorker();
+class MyServiceWorker extends UVServiceWorker {
+  async fetch(event) {
+    // You can add here any additional logic you want to implement
+    // before delegating the fetch request to the UVServiceWorker.
+    return await super.fetch(event);
+  }
+}
 
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    (new MyServiceWorker()).fetch(event)
+  );
+});
