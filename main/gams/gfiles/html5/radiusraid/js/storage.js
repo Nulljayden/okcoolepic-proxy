a001292665e2ffd27d254ab73963f488ee177275
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:42 -0500

    improve

--- main/gams/gfiles/html5/radiusraid/js/storage.js
+++ main/gams/gfiles/html5/radiusraid/js/storage.js
@@ -1,36 +1,36 @@
-// local storage helpers - source: http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage/3146971#3146971
-Storage.prototype.setObject = function( key, value ) {
-	this.setItem( key, JSON.stringify( value ) );
+// localStorage helper functions
+function setLocalStorageObject(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
-Storage.prototype.getObject = function( key ) {
-	var value = this.getItem( key );
-	return value && JSON.parse( value );
+function getLocalStorageObject(key) {
+  var value = localStorage.getItem(key);
+  return value ? JSON.parse(value) : null;
 }
 
-Storage.prototype.removeObject = function( key ) {
-	this.removeItem( key );
+function removeLocalStorageObject(key) {
+  localStorage.removeItem(key);
 }
 
 $.setupStorage = function() {
-	$.storage = localStorage.getObject( 'radiusraid' ) || {
-		'mute': 0,
-		'autofire': 0,
-		'score': 0,
-		'level': 0,		
-		'rounds': 0,
-		'kills': 0,
-		'bullets': 0,
-		'powerups': 0,		
-		'time': 0
-	};
+  $.storage = getLocalStorageObject('radiusraid') || {
+    'mute': 0,
+    'autofire': 0,
+    'score': 0,
+    'level': 0,
+    'rounds': 0,
+    'kills': 0,
+    'bullets': 0,
+    'powerups': 0,
+    'time': 0
+  };
 };
 
 $.updateStorage = function() {
-	localStorage.setObject( 'radiusraid', $.storage );
+  setLocalStorageObject('radiusraid', $.storage);
 };
 
 $.clearStorage = function() {
-	localStorage.removeObject( 'radiusraid' );
-	$.setupStorage();
+  removeLocalStorageObject('radiusraid');
+  $.setupStorage();
 };
