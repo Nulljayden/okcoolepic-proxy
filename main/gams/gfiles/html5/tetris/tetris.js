152a19bf800874701ebaa01e7ec2f2631caeaccc
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:07 -0500

    improve

--- main/gams/gfiles/html5/tetris/tetris.js
+++ main/gams/gfiles/html5/tetris/tetris.js
@@ -1,249 +1,122 @@
+'use strict';
 
-FIELD_OFFSET_X = 180;
-FIELD_OFFSET_Y = 12;
+const FIELD_OFFSET_X = 180;
+const FIELD_OFFSET_Y = 12;
 
-function TetrisControl() {
-    var tetris = new Tetris(this);
+class Button {
+    constructor(action, image, x, y) {
+        this.action = action;
+        this.image = image;
+        this.x = x;
+        this.y = y;
+    }
 
-    this.setup = function () {
-	tetris.setup();
-    };
-    this.update = function () {
-	tetris.update();
-    };
-    this.draw = function () {
-	tetris.draw();
-    };
+    isClicked(x, y) {
+        // button click detection code here
+    }
 
-    this.restart = function() {
-	// create a new Tetris object
-	tetris = new Tetris(this);
-
-	// emulate an initial setup condition and the first loop
-	tetris.setup();
-	tetris.update();
-    };
+    draw() {
+        // button drawing code here
+    }
 }
 
-function Tetris(controller) {
-    var background = null,
-    game = null,
-    timeOffset = 0,
+class Background {
+    constructor() {
+        // background constructor code here
+    }
 
-    lastEscapeState = false,
-    startPauseTime = 0,
-    paused = false,
-    lastPaused = false,
-
-    gameOver = false,
-
-    mouseClick = null,
-
-    self = this,
-
-    continueButton = null,
-    restartButton = null,
-
-    lastTime = null,
-    dTime = null,
-
-    gameEndTty = new TtyBlock('gameEndDiv', 10, 20, 1);
-    
-
-    this.setup = function () {
-	// find the keys to stop	
-	var stoppedKeys = [],
-	curAction, i;
-	for (curAction in inputAssignments) {
-	    stoppedKeys = stoppedKeys.concat(inputAssignments[curAction]);
-	}
-	jaws.preventDefaultKeys(stoppedKeys);
-
-
-	Tetris.currentInstance = self;
-	game = new Game(inputAssignments, autoRepeatConfig, thresholdConfig);
-
-	continueButton = new Button({image: 'media/buttons/continue.png', x: 250, y: 150});
-	restartButton = new Button({image: 'media/buttons/restart.png', x: 250, y: 200});
-	
-	background = new Background();
-
-	timeOffset = (new Date()).getTime();
-    };
-
-    this.update = function() {
-	var realTime = (new Date()).getTime(),
-	escapePressed = jaws.pressed('esc'),
-	scoreObject;
-
-	if (lastTime === null) {
-	    dTime = 0;
-	    lastTime = realTime;
-	} else {
-	    dTime = realTime - lastTime;
-	    lastTime = realTime;
-	}
-	
-	if (!paused && !gameOver) {
-	    // see if the game should be pased
-	    if (escapePressed && (!lastEscapeState)) {
-		// go into pause mode
-		startPauseTime = realTime;
-		paused = true;
-	    } else {
-		game.update(realTime - timeOffset);
-		// see if the game is over
-		scoreObject = game.getResults();
-		if (scoreObject) {
-		    gameOver = true;
-
-		    // make the game end visible
-		    document.getElementById('gameEndContainer').setAttribute('class', 'gameEndOutputVisible');
-		    gameEndTty.addLine('GOOD GAME!!!');
-		    gameEndTty.addLine('');
-		    gameEndTty.addLine('');
-		    if (scoreObject.won) {
-			gameEndTty.addLine('You Win!');
-		    } else {
-			gameEndTty.addLine('Better Luck Next Time');
-		    }
-		    gameEndTty.addLine('');
-		    gameEndTty.addLine('');
-
-			/*
-		    gameEndTty.addLine('Re-directing you to');
-		    gameEndTty.addLine('the score screen...');
-			*/
-			
-			gameEndTty.addLine('Your score was:');
-			gameEndTty.addLine(scoreObject.score.toString());
-		    gameEndTty.addLine('');
-		    gameEndTty.addLine('');
-
-		    //sendScoreRequest(scoreObject.score);
-
-			window.setTimeout(function() {
-				document.getElementById('gameEndContainer').setAttribute('class', 'gameEndOutputHidden');
-				controller.restart();
-			}, 6000);
-		}
-	    }
-	} else if (paused) {
-	    // see if the escape key was hit
-	    if (escapePressed && (!lastEscapeState)) {
-		// change the time offset
-		timeOffset += realTime - startPauseTime;
-		paused = false;
-	    }
-	    // see if any buttons were pressed
-	    if (mouseClick) {
-		if (continueButton.isClicked(mouseClick.x, mouseClick.y)) {
-		    // change the time offset
-		    timeOffset += realTime - startPauseTime;
-		    paused = false;
-		}
-		if (restartButton.isClicked(mouseClick.x, mouseClick.y)) {
-		    // restart the game
-		    controller.restart();
-		    return;
-		}
-	    }
-	} else {
-	    // TODO: nothing???
-	}
-	
-	lastEscapeState = escapePressed;
-	mouseClick = null;
-    };
-
-    this.draw = function() {
-
-	if (!paused && !gameOver) {
-
-	    // draw the game
-	    background.draw(lastPaused);
-	    if (lastPaused) {
-		lastPaused = false;
-		Block.invalidateAll();
-	    }
-	    game.draw(dTime);
-	    Block.invalidFlushed();
-
-	} else if (paused) {
-	    // draw the game
-	    background.draw();
-	    game.draw(dTime);
-
-	    //draw the pause menu
-	    continueButton.draw();
-	    restartButton.draw();
-	    lastPaused = true;
-	} else {
-	    // continue to draw the game for game over
-	    // draw the game
-	    background.draw();
-	    game.draw(dTime);
-	}
-
-	gameEndTty.draw(dTime);
-    };
-    
-    this.mouseClicked = function(x, y) {
-	mouseClick = {x: x, y: y};
-    };
+    draw(lastPaused) {
+        // background drawing code here
+    }
 }
 
-window.onload = function () {
-    loadGameControls();
+class TtyBlock {
+    constructor(elementId, width, height, padding) {
+        // tty block constructor code here
+    }
 
-    jaws.assets.add('media/blueblock.png');
-    jaws.assets.add('media/cyanblock.png');
-    jaws.assets.add('media/greenblock.png');
-    jaws.assets.add('media/orangeblock.png');
-    jaws.assets.add('media/purpleblock.png');
-    jaws.assets.add('media/redblock.png');
-    jaws.assets.add('media/yellowblock.png');
+    addLine(line) {
+        // tty block add line code here
+    }
 
-    jaws.assets.add('media/greyblock.png');
-    jaws.assets.add('media/emptyblock.png');
+    draw(dTime) {
+        // tty block drawing code here
+    }
+}
 
-    jaws.assets.add('media/buttons/continue.png');
-    jaws.assets.add('media/buttons/restart.png');
+class Game {
+    constructor(inputAssignments, autoRepeatConfig, thresholdConfig) {
+        // game constructor code here
+    }
 
-    jaws.assets.add('media/background/backdrop.png');
-    jaws.assets.add('media/background/topbar.png');
+    update(realTime) {
+        // game update code here
+    }
 
-    jaws.start(TetrisControl);
+    draw(dTime) {
+        // game drawing code here
+    }
+
+    getResults() {
+        // game get results code here
+    }
+}
+
+const inputAssignments = {
+    // input assignments here
 };
 
-var redirCode;
+const autoRepeatConfig = {
+    // auto repeat config here
+};
 
-function redirectToScore() {
-    window.location.replace('/scoreScreen.html?tempRef=' + redirCode);
+const thresholdConfig = {
+    // threshold config here
+};
+
+class TetrisControl {
+    constructor() {
+        this.tetris = new Tetris(this);
+    }
+
+    setup() {
+        this.tetris.setup();
+    }
+
+    update() {
+        this.tetris.update();
+    }
+
+    draw() {
+        this.tetris.draw();
+    }
+
+    restart() {
+        this.tetris = new Tetris(this);
+        this.tetris.setup();
+        this.tetris.update();
+    }
 }
 
-function sendScoreRequest(score) {
-    var xmlhttp;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-	xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	{
-	    redirCode = xmlhttp.responseText;
+class Tetris {
+    constructor(controller) {
+        this.background = null;
+        this.game = null;
+        this.timeOffset = 0;
 
-	    setTimeout('redirectToScore();', 4000);
-	}
-    }
-    
-    // World's 3rd most piss-poor obfustication technique
-    // A serious real-time/replay game monitor is needed
-    xmlhttp.open("POST", "/score/reportScore?gthbyu="+(score*17), true);
-    xmlhttp.send();
-}
+        this.lastEscapeState = false;
+        this.startPauseTime = 0;
+        this.paused = false;
+        this.lastPaused = false;
+
+        this.gameOver = false;
+
+        this.mouseClick = null;
+
+        this.continueButton = null;
+        this.restartButton = null;
+
+        this.lastTime = null;
+        this.dTime = null;
+
+        this.gameEndTty = new TtyBlock('
