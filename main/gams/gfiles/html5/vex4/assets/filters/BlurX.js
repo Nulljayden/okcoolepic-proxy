227cc867fe71d0be257d2873b08bbd77f98b1a5d
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:12 -0500

    improve

--- main/gams/gfiles/html5/vex4/assets/filters/BlurX.js
+++ main/gams/gfiles/html5/vex4/assets/filters/BlurX.js
@@ -1,6 +1,23 @@
 /**
-* A horizontal blur filter by Mat Groves http://matgroves.com/ @Doormat23
-*/
+ * A horizontal blur filter for Phaser.
+ * 
+ * To use this filter, create a new instance of it and add it to a Phaser.Game object:
+ * 
+ *     var blurFilter = new Phaser.Filter.BlurX(game);
+ *     game.filterManager.add(blurFilter);
+ * 
+ * Then, apply the filter to a game object's render texture:
+ * 
+ *     gameObject.filters = [blurFilter];
+ * 
+ * The blur amount can be adjusted using the `blur` property:
+ * 
+ *     blurFilter.blur = 10;
+ * 
+ * Or by calling the `setBlur` method:
+ * 
+ *     blurFilter.setBlur(10);
+ */
 Phaser.Filter.BlurX = function (game) {
 
     Phaser.Filter.call(this, game);
@@ -9,45 +26,20 @@ Phaser.Filter.BlurX = function (game) {
 
     this.fragmentSrc = [
 
-      "precision mediump float;",
-      "varying vec2 vTextureCoord;",
-      "varying vec4 vColor;",
-      "uniform float blur;",
-      "uniform sampler2D uSampler;",
+        "precision mediump float;",
+        "varying vec2 vTextureCoord;",
+        "varying vec4 vColor;",
+        "uniform float blur;",
+        "uniform sampler2D uSampler;",
 
         "void main(void) {",
 
-          "vec4 sum = vec4(0.0);",
+            "vec4 sum = vec4(0.0);",
 
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - 4.0*blur, vTextureCoord.y)) * 0.05;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - 3.0*blur, vTextureCoord.y)) * 0.09;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - 2.0*blur, vTextureCoord.y)) * 0.12;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x - blur, vTextureCoord.y)) * 0.15;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y)) * 0.16;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + blur, vTextureCoord.y)) * 0.15;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + 2.0*blur, vTextureCoord.y)) * 0.12;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + 3.0*blur, vTextureCoord.y)) * 0.09;",
-          "sum += texture2D(uSampler, vec2(vTextureCoord.x + 4.0*blur, vTextureCoord.y)) * 0.05;",
-
-          "gl_FragColor = sum;",
-
-        "}"
-    ];
-
-};
-
-Phaser.Filter.BlurX.prototype = Object.create(Phaser.Filter.prototype);
-Phaser.Filter.BlurX.prototype.constructor = Phaser.Filter.BlurX;
-
-Object.defineProperty(Phaser.Filter.BlurX.prototype, 'blur', {
-
-    get: function() {
-        return this.uniforms.blur.value / (1/7000);
-    },
-
-    set: function(value) {
-        this.dirty = true;
-        this.uniforms.blur.value = (1/7000) * value;
-    }
-
-});
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x - 4.0*blur, vTextureCoord.y)) * 0.05;",
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x - 3.0*blur, vTextureCoord.y)) * 0.09;",
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x - 2.0*blur, vTextureCoord.y)) * 0.12;",
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x - blur, vTextureCoord.y)) * 0.15;",
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y)) * 0.16;",
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x + blur, vTextureCoord.y)) * 0.15;",
+            "sum += texture2D(uSampler, vec2(vTextureCoord.x + 2.0*bl
