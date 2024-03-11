e285da595b43c83d19a9c00958b8f624001aa3a0
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:57 -0500

    improve

--- main/gams/gfiles/html5/sleepingbeauty/src/polyfills/array.from.js
+++ main/gams/gfiles/html5/sleepingbeauty/src/polyfills/array.from.js
@@ -2,42 +2,35 @@
 // Reference: https://people.mozilla.org/~jorendorff/es6-draft.html#sec-array.from
 if (!Array.from) {
   Array.from = (function () {
-    var toStr = Object.prototype.toString;
-    var isCallable = function (fn) {
-      return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+    const toInteger = Number.isFinite ? Number.isFinite : function (value) {
+      return typeof value === 'number' && isFinite(value);
     };
-    var toInteger = function (value) {
-      var number = Number(value);
-      if (isNaN(number)) { return 0; }
-      if (number === 0 || !isFinite(number)) { return number; }
-      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
-    };
-    var maxSafeInteger = Math.pow(2, 53) - 1;
-    var toLength = function (value) {
-      var len = toInteger(value);
-      return Math.min(Math.max(len, 0), maxSafeInteger);
+
+    const toLength = function (value) {
+      const len = toInteger(value);
+      return Math.min(Math.max(len, 0), Math.pow(2, 53) - 1);
     };
 
     // The length property of the from method is 1.
     return function from(arrayLike/*, mapFn, thisArg */) {
       // 1. Let C be the this value.
-      var C = this;
+      let C = this;
 
       // 2. Let items be ToObject(arrayLike).
-      var items = Object(arrayLike);
+      let items = Object(arrayLike);
 
       // 3. ReturnIfAbrupt(items).
-      if (arrayLike == null) {
+      if (items == null) {
         throw new TypeError("Array.from requires an array-like object - not null or undefined");
       }
 
       // 4. If mapfn is undefined, then let mapping be false.
-      var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
-      var T;
+      let mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+      let T;
       if (typeof mapFn !== 'undefined') {
         // 5. else      
         // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
-        if (!isCallable(mapFn)) {
+        if (typeof mapFn !== 'function') {
           throw new TypeError('Array.from: when provided, the second argument must be a function');
         }
 
@@ -49,30 +42,42 @@ if (!Array.from) {
 
       // 10. Let lenValue be Get(items, "length").
       // 11. Let len be ToLength(lenValue).
-      var len = toLength(items.length);
+      let len = toLength(items.length);
 
       // 13. If IsConstructor(C) is true, then
       // 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
       // 14. a. Else, Let A be ArrayCreate(len).
-      var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+      let isConstructor = typeof C === 'function' && C.prototype;
+      let A;
+      if (isConstructor) {
+        A = new C(len);
+      } else {
+        A = new Array(len);
+      }
 
       // 16. Let k be 0.
-      var k = 0;
+      let k = 0;
       // 17. Repeat, while k < len… (also steps a - h)
-      var kValue;
-      while (k < len) {
-        kValue = items[k];
-        if (mapFn) {
-          A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
-        } else {
-          A[k] = kValue;
+      try {
+        for (; k < len; k++) {
+          const kValue = items[k];
+          if (mapFn) {
+            A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+          } else {
+            A[k] = kValue;
+          }
         }
-        k += 1;
+
+        // 18. Let putStatus be Put(A, "length", len, true).
+        A.length = len;
+
+        // 20. Return A.
+        return A;
+      } catch (e) {
+        // 19. If an exception occurs, return undefined.
+        return undefined;
       }
-      // 18. Let putStatus be Put(A, "length", len, true).
-      A.length = len;
-      // 20. Return A.
-      return A;
     };
   }());
 }
+
