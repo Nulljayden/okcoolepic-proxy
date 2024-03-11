d4c1e931d8d7d82d8f549376fef36b3dc82f1a46
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:28:26 -0500

    comment

--- main/gams/gfiles/html5/chess/common/xhr.js
+++ main/gams/gfiles/html5/chess/common/xhr.js
@@ -1,43 +1,51 @@
 // retrieves a file via XMLHttpRequest, calls fncCallback when done or fncError on error.
 function XHR(strURL, fncCallback, ...args) {
+  // creates a new XMLHttpRequest object
   const oHTTP = new XMLHttpRequest();
+
+  // opens the connection using the GET request on the provided URL
   if (oHTTP) {
     oHTTP.open("GET", strURL, true);
+
+    // sets the response type to text
     oHTTP.responseType = "text";
+
+    // callback function for the onload event
     oHTTP.onload = function() {
+      // checks if the status code is between 200 and 400 (success)
       if (this.status >= 200 && this.status < 400) {
+        // calls the provided callback function with the response and any additional arguments
         fncCallback(this.response, ...args);
       } else {
+        // calls the error function with the error message
         fncError(this.statusText);
       }
     };
+
+    // callback function for the onerror event
     oHTTP.onerror = function() {
+      // calls the error function with the error message
       fncError("Network error");
     };
+
+    // callback function for the ontimeout event
     oHTTP.ontimeout = function() {
+      // calls the error function with the error message
       fncError("Timeout error");
     };
+
+    // sets the Content-Type header to text/plain
     oHTTP.setRequestHeader("Content-Type", "text/plain");
+
+    // sets the If-Modified-Since header to a past date
     oHTTP.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
+
+    // sends the request
     oHTTP.send();
   }
 }
 
-function setAttribs(...args) {
-  if (args.length % 2 !== 0) {
-    throw new Error("Invalid number of arguments");
-  }
-  for (let i = 0; i < args.length; i += 2) {
-    this.setAttribute(args[i], args[i + 1]);
-  }
-  return this;
-}
 
-function setStyles(...args) {
-  if (args.length % 2 !== 0) {
-    throw new Error("Invalid number of arguments");
-  }
-  for (let i = 0; i < args.length; i += 2) {
-    this.style[args[i]] = args[i + 1];
-  }
-  return this;
+
+
+// sets multiple attributes on
