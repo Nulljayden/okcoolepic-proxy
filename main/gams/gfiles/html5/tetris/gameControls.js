96cd77f1b84d4111c9014ad57a9f86cb8bc834d7
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:24:03 -0500

    improve

--- main/gams/gfiles/html5/tetris/gameControls.js
+++ main/gams/gfiles/html5/tetris/gameControls.js
@@ -1,46 +1,52 @@
-
 // default input assignments
-var inputAssignments = {
-    shiftLeft: ['left'],
-    shiftRight: ['right'],
-    softDrop: ['down'],
-    rotateLeft: ['z'],
-    rotateRight: ['x', 'up'],
-    swap: ['shift', 'c'],
-    hardDrop: ['space']
+const inputAssignments = {
+  shiftLeft: ['left'],
+  shiftRight: ['right'],
+  softDrop: ['down'],
+  rotateLeft: ['z'],
+  rotateRight: ['x', 'up'],
+  swap: ['shift', 'c'],
+  hardDrop: ['space']
 };
 
-var autoRepeatConfig = 50;
-var thresholdConfig = 200;
+const autoRepeatConfig = 50;
+const thresholdConfig = 200;
 
+// Function to load game controls from cookies
 function loadGameControls() {
-    var cookies = ['rotateLeft',
-		   'rotateRight',
-		   'shiftLeft',
-		   'shiftRight',
-		   'softDrop',
-		   'hardDrop',
-		   'swap'],
-    i, curVal;
-    
-    // if custom controls need to be loaded
-    if (readCookie('customControls') === 'TRUE') {
-	// for each input cookie
-	for (i = 0; i < cookies.length; i += 1) {
-	    // print the controls to the table
-	    curVal = readCookie(cookies[i]);
-	    document.getElementById(cookies[i]).innerHTML = curVal;
-	    // pass the controls into the config object
-	    inputAssignments[cookies[i]] = [curVal.toLowerCase()];
-	}
-    }
+  // List of cookie names
+  const cookies = [
+    'rotateLeft',
+    'rotateRight',
+    'shiftLeft',
+    'shiftRight',
+    'softDrop',
+    'hardDrop',
+    'swap'
+  ];
 
-    var autoRepeat = readCookie('autoRepeat');
-    if (autoRepeat !== null) {
-	autoRepeatConfig = parseInt(autoRepeat);
-    }
-    var threshold = readCookie('threshold');
-    if (threshold != null) {
-	thresholdConfig = parseInt(threshold);
-    }
-}
+  // If custom controls need to be loaded
+  if (getCookie('customControls') === 'TRUE') {
+    // For each input cookie
+    cookies.forEach((cookie) => {
+      // Get the cookie value
+      const curVal = getCookie(cookie);
+
+      // If the cookie value is not empty
+      if (curVal) {
+        // Print the controls to the table
+        document.getElementById(cookie).innerHTML = curVal;
+
+        // Pass the controls into the config object
+        inputAssignments[cookie] = [curVal.toLowerCase()];
+      }
+    });
+  }
+
+  // Get the autoRepeat cookie value
+  const autoRepeat = getCookie('autoRepeat');
+
+  // If the autoRepeat cookie value is not null
+  if (autoRepeat !== null) {
+    // Set the autoRepeatConfig value
+
