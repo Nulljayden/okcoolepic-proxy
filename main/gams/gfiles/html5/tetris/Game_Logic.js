de7221bb4e9fa375547359614e6f496fce13f2f3
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:08 -0500

    comment

--- main/gams/gfiles/html5/tetris/Game_Logic.js
+++ main/gams/gfiles/html5/tetris/Game_Logic.js
@@ -1,23 +1,17 @@
-
 /**
-* @returns {[Number]} the line numbers of all the completed rows
-*/ 
+ * Returns the line numbers of all the completed rows.
+ * @returns {[Number]} the line numbers of all the completed rows
+ */
 Game.prototype.getRows = function () {
-    var i,
-    rows = [],
-    res = [],
-    curRow;
-
-    // initialize the rows to 0
-    for (i = 0; i < 20; i += 1) {
+    // Initialize the rows to 0
+    for (var i = 0; i < 20; i += 1) {
 	rows[i] = 0;
     }
-    // for each block
-    for (i = 0; i < this.blocks.length; i += 1) {
-	// increment the appropriate row
-	curRow = this.blocks[i].getY();
+    // Increment the appropriate row for each block
+    for (var i = 0; i < this.blocks.length; i += 1) {
+	var curRow = this.blocks[i].getY();
 	rows[curRow] += 1;
-	// if the row is full
+	// If the row is full, add it to the result array
 	if (rows[curRow] === 10) {
 	    res.push(curRow);
 	}
@@ -27,42 +21,33 @@ Game.prototype.getRows = function () {
 };
 
 /**
-* Removes the rows from the field
-*/
+ * Removes the rows from the field.
+ */
 Game.prototype.removeRows = function (rows) {
-    var dropDist = {},
-    i, j,
-    remove = {},
-    curBlock,
-    curY;
-
-    // initialize drops to 0
-    for (i = -4; i < 20; i += 1) {
+    // Initialize drops to 0
+    for (var i = -4; i < 20; i += 1) {
 	dropDist[i] = 0;
     }
 
-    // for each removed row
-    for (i = 0; i < rows.length; i += 1) {
+    // For each removed row, increment the drop distance for rows above it
+    for (var i = 0; i < rows.length; i += 1) {
 	remove[rows[i]] = true;
-	
-	// every row above this should be dropped another spot
-	for (j = -4; j < rows[i]; j += 1) {
+	for (var j = -4; j < rows[i]; j += 1) {
 	    dropDist[j] += 1;
 	}
     }
 
-    // for each block
-    for (i = 0; i < this.blocks.length; i += 1) {
-	curBlock = this.blocks[i];
-	curY = curBlock.getY();
+    // For each block, drop it or remove it if it's in a removed row
+    for (var i = 0; i < this.blocks.length; i += 1) {
+	var curBlock = this.blocks[i];
+	var curY = curBlock.getY();
 
-	// if it is being removed
+	// If it is being removed, remove the block and decrement i
 	if (remove[curY]) {
-	    // remove the block
 	    this.removeBlock(i);
 	    i -= 1;
 	} else {
-	    // it is being dropped
+	    // Otherwise, drop the block
 	    curBlock.setPosition(curBlock.getX(), curBlock.getY() + dropDist[curY]);
 	}
     }
@@ -76,32 +61,31 @@ Game.prototype.removeBlock = function(index) {
 Game.prototype.applyGravity = function (dTime) {
     this.timeToNextDrop -= dTime;
 
-    // drop until there is a positive time until the next drop time is positive, or the control group s bottomed out
+    // Drop the block until the next drop time is positive or the control group has bottomed out
     while (this.timeToNextDrop < 0 && (!this.controlGroup.isBottomed())) {
 	this.dropBlock(true);
 	this.timeToNextDrop += this.dropPeriod;
     }
 
-    // if it exited through bottoming, reset the drop period
+    // If the control group has bottomed out, reset the drop period
     if (this.controlGroup.isBottomed()) {
 	this.timeToNextDrop = this.dropPeriod;
     }
 };
 
 /**
-* Changes the shapes of the preview along the side
-* @param {[Char]} queue - the queue of pieces
-*/
+ * Changes the shapes of the preview along the side.
+ * @param {[Char]} queue - the queue of pieces
+ */
 Game.prototype.updatePreviews = function(queue) {
-    var i;
-    for (i = 0; i < queue.length; i += 1) {
+    for (var i = 0; i < queue.length; i += 1) {
 	this.previewGroups[i].setShape(queue[i]);
     }
 };
 
 /**
-* called when the user attempts to swap a block
-*/
+ * called when the user attempts to swap a block
+ */
 Game.prototype.swap = function() {
     var i, j,
     newShape,
@@ -110,7 +94,7 @@ Game.prototype.swap = function() {
     newBlocks = [],
     thisObject = this;
 
-    // can only be called once per drop
+    // Can only be called once per drop
     if (!this.swapAllowed) {
 	return;
     }
@@ -119,10 +103,9 @@ Game.prototype.swap = function() {
     // Reset the locking
     this.resetLockCounter(false);
 
-    // remove the blocks
-    // for each block on the field
+    // Remove the blocks
     for (i = 0; i < this.blocks.length; i += 1) {
-	// if the block is part of the control group, remove it
+	// If the block is part of the control group, remove it
 	for (j = 0; j < 4; j += 1) {
 	    if (oldBlocks[j] === this.blocks[i]) {
 		this.removeBlock(i);
@@ -131,7 +114,7 @@ Game.prototype.swap = function() {
 	}
     }
     
-    // if there is a block waiting
+    // If there is a block waiting
     if (this.swapGroup) {
 	newShape = this.swapGroup.getShape();
 	for (i = 0; i < 4; i += 1) {
@@ -148,7 +131,7 @@ Game.prototype.swap = function() {
 	return;
     }
 
-    // if there is no block waiting
+    // If there is no block waiting
     this.swapGroup = new PreviewGroup(-100, 60);
     this.swapGroup.setShape(oldShape);
     this.newBlock(true);    
@@ -159,7 +142,7 @@ Game.prototype.swap = function() {
 * locks the currnt piece in, registers lines and makes a new block
 */
 Game.prototype.lockBlocks = function() {
-    // figure out if it a t-spin/t-spin mini
+    // Figure out if it a t-spin/t-spin mini
     var tSpinType = this.controlGroup.getTSpin(),
     scoreObject = {},
     rows;
@@ -170,40 +153,12 @@ Game.prototype.lockBlocks = function() {
 	scoreObject.normalT = true;
     }
 
-    // look for rows
+    // Look for rows
     rows = this.getRows();
     scoreObject.lines = rows.length;
     if (rows.length > 0) {
 	this.removeRows(rows);
     }
 
-    // apply the score
-    this.scoreTracker.updateScore(scoreObject);
-
-    this.newBlock();
-    this.resetLockCounter(false);
-};
-
-/**
-* Resets the lock counter, and the slide counter if not soft
-* @param {Boolean} soft = true if a soft reset, and the slide counter should not be reset
-*/
-Game.prototype.resetLockCounter = function (soft) {
-    if (soft) {
-	this.slideCount += 1;
-    } else {
-	this.slideCount = 0;
-    }
-    this.bottomTimer = this.bottomLockTime;
-};
-
-/**
- * Determines if the game is over and returns a score object
- * if it is. Otherwise, returns null
- */
-Game.prototype.getResults = function() {
-    if (this.gameLost || this.scoreTracker.gameWon()) {
-	return this.scoreTracker.getResults();
-    }
-    return null;
-};
+    // Apply the score
+    this.scoreTracker.updateScore(scoreObject
