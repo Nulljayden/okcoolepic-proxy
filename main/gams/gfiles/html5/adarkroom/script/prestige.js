7fd2bd48b5b1cfaf232b4211059457fd34749baa
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:29 -0500

    improve

--- main/gams/gfiles/html5/adarkroom/script/prestige.js
+++ main/gams/gfiles/html5/adarkroom/script/prestige.js
@@ -1,103 +1,84 @@
+/**
+ * Prestige object for managing player prestige data.
+ * @namespace Prestige
+ */
 var Prestige = {
-		
-	name: 'Prestige',
 
-	options: {},
+    /**
+     * The name of the prestige object.
+     * @type {string}
+     */
+    name: 'Prestige',
 
-	init: function(options) {
-		this.options = $.extend(this.options, options);
-	},
-	
-	storesMap: [
-		{ store: 'wood', type: 'g' },
-		{ store: 'fur', type: 'g' },
-		{ store: 'meat', type: 'g' },
-		{ store: 'iron', type: 'g' },
-		{ store: 'coal', type: 'g' },
-		{ store: 'sulphur', type: 'g' },
-		{ store: 'steel', type: 'g' },
-		{ store: 'cured meat', type: 'g' },
-		{ store: 'scales', type: 'g' },
-		{ store: 'teeth', type: 'g' },
-		{ store: 'leather', type: 'g' },
-		{ store: 'bait', type: 'g' },
-		{ store: 'torch', type: 'g' },
-		{ store: 'cloth', type: 'g' },
-		{ store: 'bone spear', type: 'w' },
-		{ store: 'iron sword', type: 'w' },
-		{ store: 'steel sword', type: 'w' },
-		{ store: 'bayonet', type: 'w' },
-		{ store: 'rifle', type: 'w' },
-		{ store: 'laser rifle', type: 'w' },
-		{ store: 'bullets', type: 'a' },
-		{ store: 'energy cell', type: 'a' },
-		{ store: 'grenade', type: 'a' },
-		{ store: 'bolas', type: 'a' }
-	],
-	
-	getStores: function(reduce) {
-		var stores = [];
-		
-		for(var i in this.storesMap) {
-			var s = this.storesMap[i];
-			stores.push(Math.floor($SM.get('stores["' + s.store + '"]', true) / 
-					(reduce ? this.randGen(s.type) : 1)));
-		}
-		
-		return stores;
-	},
-	
-	get: function() {
-		return {
-			stores: $SM.get('previous.stores'),
-			score: $SM.get('previous.score')
-		};
-	},
-	
-	set: function(prestige) {
-		$SM.set('previous.stores', prestige.stores);
-		$SM.set('previous.score', prestige.score);
-	},
-	
-	save: function() {
-		$SM.set('previous.stores', this.getStores(true));
-		$SM.set('previous.score', Score.totalScore());
-	},
-  
-	collectStores : function() {
-		var prevStores = $SM.get('previous.stores');
-		if(prevStores != null) {
-			var toAdd = {};
-			for(var i in this.storesMap) {
-				var s = this.storesMap[i];
-				toAdd[s.store] = prevStores[i];
-			}
-			$SM.addM('stores', toAdd);
-			
-			// Loading the stores clears em from the save
-			prevStores.length = 0;
-		}
-	},
+    /**
+     * The options for the prestige object.
+     * @type {object}
+     */
+    options: {},
 
-	randGen : function(storeType) {
-		var amount;
-		switch(storeType) {
-		case 'g':
-			amount = Math.floor(Math.random() * 10);
-			break;
-		case 'w':
-			amount = Math.floor(Math.floor(Math.random() * 10) / 2);
-			break;
-		case 'a':
-			amount = Math.ceil(Math.random() * 10 * Math.ceil(Math.random() * 10));
-			break;
-		default:
-			return 1;
-		}
-		if (amount !== 0) {
-			return amount;
-		}
-		return 1;
-	}
+    /**
+     * The map of stores for the prestige object.
+     * @type {array}
+     */
+    storesMap: [
+        { store: 'wood', type: 'g' },
+        { store: 'fur', type: 'g' },
+        { store: 'meat', type: 'g' },
+        { store: 'iron', type: 'g' },
+        { store: 'coal', type: 'g' },
+        { store: 'sulphur', type: 'g' },
+        { store: 'steel', type: 'g' },
+        { store: 'cured meat', type: 'g' },
+        { store: 'scales', type: 'g' },
+        { store: 'teeth', type: 'g' },
+        { store: 'leather', type: 'g' },
+        { store: 'bait', type: 'g' },
+        { store: 'torch', type: 'g' },
+        { store: 'cloth', type: 'g' },
+        { store: 'bone spear', type: 'w' },
+        { store: 'iron sword', type: 'w' },
+        { store: 'steel sword', type: 'w' },
+        { store: 'bayonet', type: 'w' },
+        { store: 'rifle', type: 'w' },
+        { store: 'laser rifle', type: 'w' },
+        { store: 'bullets', type: 'a' },
+        { store: 'energy cell', type: 'a' },
+        { store: 'grenade', type: 'a' },
+        { store: 'bolas', type: 'a' }
+    ],
 
-};
+    /**
+     * Initializes the prestige object with options.
+     * @param {object} options - The options for the prestige object.
+     */
+    init: function(options) {
+        this.options = $.extend(this.options, options);
+    },
+
+    /**
+     * Gets the stores for the prestige object.
+     * @param {boolean} reduce - Whether to reduce the stores or not.
+     * @return {array} - The array of stores.
+     */
+    getStores: function(reduce) {
+        var stores = this.storesMap.map(function(s) {
+            var amount = $SM.get('stores["' + s.store + '"]', true);
+            return reduce ? Math.floor(amount / this.randGen(s.type)) : amount;
+        }, this);
+        return stores;
+    },
+
+    /**
+     * Gets the previous prestige data.
+     * @return {object} - The previous prestige data.
+     */
+    get: function() {
+        return {
+            stores: $SM.get('previous.stores'),
+            score: $SM.get('previous.score')
+        };
+    },
+
+    /**
+     * Sets the previous prestige data.
+     * @param {object} prestige - The previous
