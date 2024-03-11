bbb0d48c0ce72cbb4ee86ac758a3e75db035f933
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:58 -0500

    improve

--- main/gams/gfiles/html5/towermaster/src/hook.js
+++ main/gams/gfiles/html5/towermaster/src/hook.js
@@ -1,54 +1,69 @@
-import { getSwingBlockVelocity } from './utils'
-import * as constant from './constant'
+import { getSwingBlockVelocity } from './utils';
+import * as constant from './constant';
 
-export const hookAction = (instance, engine, time) => {
-  const ropeHeight = engine.getVariable(constant.ropeHeight)
+export type HookInstance = {
+  x: number;
+  y: number;
+  angle: number;
+  weightX: number;
+  weightY: number;
+  ready: boolean;
+};
+
+export const hookAction = (instance: HookInstance, engine, time: number) => {
+  const ropeHeight = engine.getVariable(constant.ropeHeight);
+
   if (!instance.ready) {
-    instance.x = engine.width / 2
-    instance.y = ropeHeight * -1.5
-    instance.ready = true
+    instance.x = engine.width / 2;
+    instance.y = ropeHeight * -1.5;
+    instance.ready = true;
   }
+
   engine.getTimeMovement(
     constant.hookUpMovement,
     [[instance.y, instance.y - ropeHeight]],
     (value) => {
-      instance.y = value
+      instance.y = value;
     },
     {
       after: () => {
-        instance.y = ropeHeight * -1.5
-      }
+        instance.y = ropeHeight * -1.5;
+      },
     }
-  )
+  );
+
   engine.getTimeMovement(
     constant.hookDownMovement,
     [[instance.y, instance.y + ropeHeight]],
     (value) => {
-      instance.y = value
+      instance.y = value;
     },
     {
-      name: 'hook'
+      name: 'hook',
     }
-  )
-  const initialAngle = engine.getVariable(constant.initialAngle)
-  instance.angle = initialAngle *
-    getSwingBlockVelocity(engine, time)
-  instance.weightX = instance.x +
-    (Math.sin(instance.angle) * ropeHeight)
-  instance.weightY = instance.y +
-    (Math.cos(instance.angle) * ropeHeight)
-}
+  );
 
-export const hookPainter = (instance, engine) => {
-  const { ctx } = engine
-  const ropeHeight = engine.getVariable(constant.ropeHeight)
-  const ropeWidth = ropeHeight * 0.1
-  const hook = engine.getImg('hook')
-  ctx.save()
-  ctx.translate(instance.x, instance.y)
-  ctx.rotate((Math.PI * 2) - instance.angle)
-  ctx.translate(-instance.x, -instance.y)
-  engine.ctx.drawImage(hook, instance.x - (ropeWidth / 2), instance.y, ropeWidth, ropeHeight + 5)
-  ctx.restore()
-}
+  const initialAngle = engine.getVariable(constant.initialAngle);
+  if (typeof initialAngle !== 'number') {
+    console.error('Invalid initialAngle value:', initialAngle);
+    return;
+  }
 
+  instance.angle = initialAngle * getSwingBlockVelocity(engine, time);
+
+  instance.weightX = instance.x + Math.sin(instance.angle) * ropeHeight;
+  instance.weightY = instance.y + Math.cos(instance.angle) * ropeHeight;
+};
+
+export const hookPainter = (instance: HookInstance, engine) => {
+  const { ctx } = engine;
+  const ropeHeight = engine.getVariable(constant.ropeHeight);
+  const ropeWidth = ropeHeight * 0.1;
+  const hook = engine.getImg('hook');
+
+  if (!hook) {
+    console.error('Missing hook image');
+    return;
+  }
+
+  ctx
