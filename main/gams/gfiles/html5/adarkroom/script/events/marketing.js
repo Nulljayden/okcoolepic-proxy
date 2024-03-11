a44577a3a0c963270ebdbb7a5414afcbee8c087a
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:23:22 -0500

    improve

--- main/gams/gfiles/html5/adarkroom/script/events/marketing.js
+++ main/gams/gfiles/html5/adarkroom/script/events/marketing.js
@@ -1,35 +1,39 @@
 /**
- Module for triggering marketing messages
- @author mtownsend
- @since Jan 2021
-*/
+ * Module for triggering marketing messages
+ * @author mtownsend
+ * @since Jan 2021
+ */
 
-Events.Marketing = [{
-  /* Play Penrose! */
-  title: _('Penrose'),
-  isAvailable: () => !$SM.get('marketing.penrose'),
-  scenes: {
-    'start': {
-      text: [
-        _('a strange thrumming, pounding and crashing. visions of people and places, of a huge machine and twisting curves.'),
-        _('inviting. it would be so easy to give in, completely.')
-      ],
-      notification: _('a strange thrumming, pounding and crashing. and then gone.'),
-      blink: true,
-      buttons: {
-        'give in': {
-          text: _('give in'),
-          onClick: () => {
-            $SM.set('marketing.penrose', true);
-          },
-          link: 'https://penrose.doublespeakgames.com/?utm_source=adarkroom&utm_medium=crosspromote&utm_campaign=event'
+// Check if the Events object exists
+if (typeof Events !== 'undefined') {
+  // Add the marketing module to the Events object
+  Events.Marketing = Events.Marketing || [];
+
+  // Define the marketing module
+  const marketingModule = {
+    /* Play Penrose! */
+    title: _('Penrose'),
+    weight: 1,
+    isAvailable: () => !$SM.get('marketing.penrose'),
+    scenes: {
+      'start': {
+        text: [
+          _('a strange thrumming, pounding and crashing. visions of people and places, of a huge machine and twisting curves.'),
+          _('inviting. it would be so easy to give in, completely.')
+        ],
+        notification: {
+          text: _('a strange thrumming, pounding and crashing. and then gone.'),
+          duration: 3000
         },
-        'ignore': {
-          text: _('ignore it'),
-          nextScene: 'end'
-        }
-      }
-    }
-  },
-  audio: AudioLibrary.EVENT_NOISES_INSIDE
-}];
+        blink: true,
+        buttons: {
+          'give in': {
+            text: _('give in'),
+            onClick: () => {
+              triggerMarketingMessage('penrose');
+            },
+            link: 'https://penrose.doublespeakgames.com/?utm_source=adarkroom&utm_medium=crosspromote&utm_campaign=event'
+          },
+          'ignore': {
+            text: _('ignore it'),
+
