856d66483829b0230ae5328e6469454e15309632
Jayden <99763670+Nulljayden@users.noreply.github.com>
2024-03-11 09:29:01 -0500

    comment

--- main/gams/gfiles/html5/spacecompany/solCenter.js
+++ main/gams/gfiles/html5/spacecompany/solCenter.js
@@ -1,32 +1,45 @@
 // Sol Center Tab
 
+// takes the specified resources
 function takeResources(resources) {
+  // iterate through the resources object
   for (const [resource, amount] of Object.entries(resources)) {
+    // subtract the specified amount of the resource from the game's resources
     Game.resources.takeResource(resource, amount);
   }
 }
 
+// unlocks the specified technology if it can be unlocked
 function unlockTech(techName) {
   if (Game.tech.canUnlock(techName)) {
+    // unlock the technology
     Game.tech.unlockTech(techName);
+    // call the newUnlock function with the argument "research"
     newUnlock("research");
+    // add the technology name to the buttonsHidden array
     buttonsHidden.push(techName);
   }
 }
 
+// updates the research element and unlock element based on the researchId
 function updateResearch(researchId, classNameHidden, classNameVisible) {
   const researchElement = document.getElementById(researchId);
   const unlockElement = document.getElementById(researchId.replace("research", "unlock"));
 
   if (getResourcesSufficient(researchId)) {
+    // subtract the resource costs for the researchId
     takeResources(getResourceCosts(researchId));
+    // set the className of the research element to classNameHidden
     researchElement.className = classNameHidden;
+    // set the className of the unlock element to classNameVisible
     unlockElement.className = classNameVisible;
   } else {
+    // notify the user of insufficient resources
     Game.notify("Insufficient resources to unlock " + researchId.replace("research", ""));
   }
 }
 
+// checks if the player has sufficient resources for the researchId
 function getResourcesSufficient(researchId) {
   const costs = getResourceCosts(researchId);
   for (const [resource, amount] of Object.entries(costs)) {
@@ -37,6 +50,7 @@ function getResourcesSufficient(researchId) {
   return true;
 }
 
+// returns the resource costs for the researchId
 function getResourceCosts(researchId) {
   let costs;
   switch (researchId) {
@@ -58,12 +72,4 @@ function getResourceCosts(researchId) {
       costs = {
         Energy: 100000,
         Plasma: 10000,
-      };
-      break;
-    default:
-      throw new Error("Invalid researchId: " + researchId);
-  }
-  return costs;
-}
-
-// ... (rest of the code)
+     
